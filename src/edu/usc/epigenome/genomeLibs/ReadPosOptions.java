98b34c77927bd357af57600ea417874aa2f7205e
bberman <bberman@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2009-01-02 20:07:05 +0000

    Moved trackSnps into AlignmentPosOptions, where it belongs.
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@51 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- src/edu/usc/epigenome/genomeLibs/ReadPosOptions.java
+++ src/edu/usc/epigenome/genomeLibs/ReadPosOptions.java
@@ -3,7 +3,6 @@ package edu.usc.epigenome.genomeLibs;
 public class ReadPosOptions {
 	
 	public int minQualityScore = 0;
-	public boolean trackSnps = true;
 	public boolean trackPositions = false;
 	public boolean trackQuals = false;
 	public boolean positionQualsSolexaEncoding = false;
