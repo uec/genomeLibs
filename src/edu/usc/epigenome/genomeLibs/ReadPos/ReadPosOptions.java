f44b6580d4c6fac726f89329baa1c788d5a496f3
bberman <bberman@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2009-11-13 13:59:58 +0000

    Changed minQualityScore default to allow negative scores (which shouldn't appear normally but if they do we want to catch them)
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@151 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- src/edu/usc/epigenome/genomeLibs/ReadPos/ReadPosOptions.java
+++ src/edu/usc/epigenome/genomeLibs/ReadPos/ReadPosOptions.java
@@ -2,7 +2,7 @@ package edu.usc.epigenome.genomeLibs.ReadPos;
 
 public class ReadPosOptions {
 	
-	public int minQualityScore = 0;
+	public int minQualityScore = Integer.MIN_VALUE;
 	public boolean trackPositions = false;
 	public boolean trackQuals = false;
 	public boolean positionQualsSolexaEncoding = false;
