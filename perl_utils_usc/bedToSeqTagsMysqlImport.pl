a52e08d06ee6455bdec938926a35c8c6b555e982
bberman <bberman@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2009-12-25 01:08:12 +0000

    First checked in.
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@189 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

---
+++ perl_utils_usc/bedToSeqTagsMysqlImport.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+
+use strict;
+
+LINE: while (my $line = <STDIN>)
+{
+    chomp $line;
+    my @f = split(/\t/, $line);
+    
+    # Check valid line while we're getting the strand.
+    my $readStart;
+    my $valid = 0;
+    if ($f[5] eq "+")
+    {
+	$valid = 1;
+	$readStart = $f[1];
+    }
+    elsif ($f[5] eq "-")
+    {
+	$valid = 1;
+	$readStart = $f[2];
+    }
+    elsif ($f[5] eq ".")
+    {
+	$valid = 1;
+	$readStart = $f[1];
+    }
+
+    next LINE unless ($valid);
+
+    print join("\t", $f[0], $readStart, $f[5])."\n";
+}
