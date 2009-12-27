a7539863232e1d41c43b7a6fda050530cbd1dd9c
bberman <bberman@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2009-12-27 23:25:37 +0000

    First checked in.
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@198 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

---
+++ perl_utils_usc/splitColsByChrom.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Basename;
+use Getopt::Long;
+
+my $USAGE = "splitColsByChrom.pl --chromCol 1 --delim , --outPrefix fileout file1.txt file2.csv\nchromCol numbering starts at 1";
+
+my $delim = "\t";
+my $outPrefix = 0;
+my $chromCol = 1;
+GetOptions ('chromCol=i' => \$chromCol, 'outPrefix=s' => \$outPrefix, 'delimx=s' => \$delim) || die "$USAGE\n";
+
+
+die "$USAGE\n" unless (@ARGV>0);
+foreach my $fn (@ARGV)
+{
+    my ($name, $path, $suf) = fileparse($fn, qr/\.[^.]*/);
+
+	$outPrefix = $name unless ($outPrefix);
+	print STDERR "chromCol=$chromCol\tprefix=$outPrefix\tdelim=\"$delim\"\tfile=$name\n";
+	
+	die "Can't read $fn\n" unless (open(F,$fn));
+	my $outfhByChrom = {};
+	
+	
+	while (my $line = <F>)
+	{
+		chomp $line;
+		my @flds = split($delim, $line);
+		
+		my $chr = $flds[$chromCol-1];
+		$chr = "chr${chr}" unless ($chr =~ /^chr/i);
+		$flds[$chromCol-1] = $chr;
+		
+		my $fh = $outfhByChrom->{$chr};
+		if ($fh)
+		{
+			print STDERR "Found fh for chr $chr: $fh\n";	
+		}
+		else
+		{
+			print STDERR "Didn't find fh for chr $chr\n";	
+			my $outfn = "${outPrefix}${chr}.txt";
+			die "Can't write to $outfn\n" unless (open($fh, ">$outfn"));
+			$outfhByChrom->{$chr} = $fh;
+		}
+		
+		print $fh join($delim, @flds)."\n";
+	}
+	close(F);
+
+	
+	# Close output files	
+	foreach my $outfh (values(%$outfhByChrom))
+	{
+		close($outfh);
+	}
+	
+
+}
