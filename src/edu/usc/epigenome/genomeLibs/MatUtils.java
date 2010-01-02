7522ff262d98504d96eeab091884c86730a27f97
bberman <bberman@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-01-02 00:08:28 +0000

    Added a few things.  Also now sumMats will work correctly to preserve NaNs in input mats.
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@222 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- src/edu/usc/epigenome/genomeLibs/MatUtils.java
+++ src/edu/usc/epigenome/genomeLibs/MatUtils.java
@@ -1,6 +1,10 @@
 package edu.usc.epigenome.genomeLibs;
 
 import java.io.*;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.logging.Logger;
 
 import org.apache.commons.math.stat.StatUtils;
 
@@ -19,6 +23,7 @@ public class MatUtils {
 		for (int i = startInd; i < (startInd+len) ; i++)
 		{
 			double val = arr[i];
+			//System.err.printf("\t\tFound val=%f\n",val);
 			if (!Double.isNaN(val))
 			{
 				total += val;
@@ -73,7 +78,7 @@ public class MatUtils {
 	public static double nanSum(double[] arr, int startInd, int len)
 	{
 		double total = 0.0;
-		double count = 0.0;
+		boolean found = false;
 		
 		for (int i = startInd; i < (startInd+len) ; i++)
 		{
@@ -81,11 +86,11 @@ public class MatUtils {
 			if (!Double.isNaN(val))
 			{
 				total += val;
-				count += 1.0;
+				found = true;
 			}
 		}
 		
-		return (count == 0.0) ? Double.NaN : total;
+		return (!found) ? Double.NaN : total;
 	}
 
 
@@ -104,6 +109,30 @@ public class MatUtils {
 		for (int i = 0; i < mat.length; i++)
 		{
 			if (Double.isNaN(mat[i])) mat[i] = inVal;
+		}
+	}
+
+	public static int countNans(double[][] mat)
+	{
+		int total = 0;
+		for (int i = 0; i < mat.length; i++)
+		{
+			for (int j = 0; j < mat[0].length; j++)
+			{
+				if (Double.isNaN(mat[i][j])) total++;
+			}
+		}
+		return total;
+	}
+	
+	public static void nansToVal(double[][] mat, double inVal)
+	{
+		for (int i = 0; i < mat.length; i++)
+		{
+			for (int j = 0; j < mat[0].length; j++)
+			{
+				if (Double.isNaN(mat[i][j])) mat[i][j] = inVal;
+			}
 		}
 	}
 
@@ -165,6 +194,48 @@ public class MatUtils {
 			}
 		}
 	}
+	
+	
+	public static double[][] sortRows(double[][] in)
+	{
+		int nr = in.length;
+		int nc = in[0].length;
+		
+		double[][] out = new double[nr][];
+		SortedMap<Double,double[]> sorter = new TreeMap<Double,double[]>();
+		for (int i = 0; i < nr; i++)
+		{
+			double mean = MatUtils.nanMean(in[i]);
+			
+			// A map will lose rows with identical means.  So we have to do some
+			// tricks to keep keys unique
+			Double key = (Double.isNaN(mean)) ? new Double(-1 * 1E10) : new Double(mean);
+			while (sorter.containsKey(key))
+			{
+				Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).info("Got duplicate key " + key + "... incrementing\n");
+				key += 1E-10;
+			}
+			
+			sorter.put(new Double(key),in[i]);
+		}
+		
+		int i = 0;
+		for (double[] row : sorter.values()) // Guaranteed to be sorted
+		{
+			out[i] = row;
+			i++;
+		}
+		
+		// Double check that we're not making a ragged matrix
+		if (i != nr)
+		{
+			System.err.printf("MatUtils.sortRows got a matrix with %d rows, but sorter only returned %d\n", nr,i);
+			System.exit(1);
+		}
+		
+		return out;
+	}
+	
 
 	public static int sumAll(int[][] mat)
 	{
@@ -330,6 +401,50 @@ public class MatUtils {
 		return out;
 	}
 	
+	public static double[][] downscaleMat(double[][] mat, int numRowsNew, int numColsNew)
+	throws Exception
+	{
+		int n = mat.length;
+		int m = mat[0].length;
+		
+		// This isn't very efficient, especially memory wise.  But it's simple code.  
+		// First rows
+		if ((numColsNew!=0) && (m!=numColsNew)) mat = MatUtils.downscaleMatRows(mat, numColsNew);
+		
+		// Then cols
+		if  ((numRowsNew!=0) && (n!=numRowsNew)) mat = MatUtils.downscaleMatCols(mat, numRowsNew);
+		
+		return mat;
+	}
+	
+	public static double[][] downscaleMatCols(double[][] in, int numRowsNew)
+	throws Exception
+	{
+		// Probably not so efficient , but hey.
+		double[][] out = MatUtils.transposedMat(MatUtils.downscaleMatRows(MatUtils.transposedMat(in), numRowsNew));
+		return out;
+	}
+	
+	public static double[][] downscaleMatRows(double[][] in, int numColsNew)
+	throws Exception
+	{
+		int n = in.length;
+		int m = in[0].length;
+		
+		int m1 = numColsNew;
+		
+		double[][] out = new double[n][];
+		for (int i = 0; i < n; i++)
+		{
+			double[] newr = new double[m1];
+			downscaleArray(newr, in[i]);
+			out[i] = newr;
+		}
+		
+		return out;
+	}
+	
+	
 	public static void downscaleArray(double[] out, double[] in)
 	throws Exception
 	{
@@ -358,7 +473,7 @@ public class MatUtils {
 			if (in_e >= in_s)
 			{
 				out[out_ind] = nanMean(in, in_s, in_e-in_s+1);
-//				System.err.println("\tout[" + out_ind + "] = mean(" + in_s + "-" + in_e +")");
+				//System.err.printf("\tout[%d]=mean(in[%d-%d])=%f\n",out_ind,in_s,in_e,out[out_ind]);
 			}
 
 //			next_in_pos = in_e+1;
@@ -391,13 +506,27 @@ public class MatUtils {
 		int ncol = mats[0][0].length;
 		
 		double[][] out = new double[nrow][ncol];
+		
+		// If input mats have NaNs, we are conservative
+		// and initialize to nan
+		MatUtils.initMat(out, Double.NaN);
+
+
 		for (int m = 0; m < nmats; m++)
 		{
 			for (int i = 0; i < nrow; i++)
 			{
 				for (int j = 0; j < ncol; j++)
 				{
-					out[i][j] += mats[m][i][j];
+
+					if (Double.isNaN(out[i][j]))
+					{
+						out[i][j] = mats[m][i][j];
+					}
+					else
+					{
+						if (!Double.isNaN(mats[m][i][j])) out[i][j] += mats[m][i][j];
+					}
 				}
 			}
 		}
@@ -536,12 +665,23 @@ public class MatUtils {
 		int nrow = in.length;
 		int ncol = in[0].length;
 		
+		System.err.printf("Transposing mat(%d,%d)\n",nrow,ncol);
+		
 		double[][] out = new double[ncol][nrow];
 		for (int i = 0; i < nrow; i++)
 		{
 			for (int j = 0; j < ncol; j++)
 			{
-				out[j][i] = in[i][j];
+				try
+				{
+					out[j][i] = in[i][j];
+				}
+				catch (Exception e)
+				{
+					System.err.printf("out[%d][%d]=in[%d][%d]\n(in[%d])=%s\nArray probably ragged\n",
+							j,i,i,j,i,""+in[i]);
+					System.exit(1);
+				}
 			}
 		}
 		
@@ -589,7 +729,14 @@ public class MatUtils {
 		{
 			for (int j = 0; j < ncol; j++)
 			{
-				total[i][j] += add[i][j];
+				if (Double.isNaN(total[i][j]))
+				{
+					total[i][j] = add[i][j];
+				}
+				else
+				{
+					if (!Double.isNaN(add[i][j])) total[i][j] += add[i][j];
+				}
 			}
 		}
 	}
