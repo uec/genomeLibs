41c0d4febc1db7653db59f13c1849cf0a560b77a
zack <zack@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-01-07 19:58:59 +0000

    fixes in solexamagick thumbnailer
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@240 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/solexaMagick.pl
+++ perl_utils_usc/solexaMagick.pl
@@ -22,14 +22,33 @@ foreach my $image (@files)
 	threads->new(\&downSample, $image);
 }
 
+#synchronize
+$semaphore->down(8);
+$semaphore->up(8);
+
 for my $laneNumber (1..8)
 {
 	for my $cycleNumber (1..200)
 	{
 		$semaphore->down();
-		threads->new(\&combine, $laneNumber, $cycleNumber);
+		threads->new(\&combineCycle, $laneNumber, $cycleNumber);
 	}
 }
+
+#synchronize
+$semaphore->down(8);
+$semaphore->up(8);
+
+
+for my $laneNumber (1..8)
+{
+	$semaphore->down();
+	&combineLane($laneNumber)	
+}
+
+#synchronize
+$semaphore->down(8);
+$semaphore->up(8);
 
 exit;
 
@@ -47,17 +66,35 @@ sub downSample
 	my $outputJpeg =  "$workDir/$lane" . "_$cycle" . "_$tile" . "_$base" . ".jpg";
 	if(!(-e $outputJpeg))
 	{
-		system("convert $tif -resize 10% $outputJpeg");
+		system("convert $tif -resize 20% $outputJpeg");
 	}
 	$semaphore->up();
 }
 
-sub combine
+sub combineCycle
 {
 	my $lane = shift @_;
 	my $cycle = shift @_;
 	$cycle = sprintf "%04d", $cycle;
-	my @cycleFiles = glob("$workDir/$lane" . "_$cycle" . "_*_a.jpg");
+	&combineCycleByBase($lane,$cycle,"a");
+	&combineCycleByBase($lane,$cycle,"c");
+	&combineCycleByBase($lane,$cycle,"g");
+	&combineCycleByBase($lane,$cycle,"t");
+	my @cycleBaseFiles = glob("$workDir/montage_$lane" . "_$cycle" . "_?.jpg");
+	my $cycleBaseString = join(' ', sort(@cycleBaseFiles));
+	my $montExec = "montage $cycleBaseString -tile 1x4 -geometry +0+0 $workDir/montage_$lane" . "_$cycle.jpg";
+	print "$montExec\n";
+	system $montExec;
+	$semaphore->up();
+}
+
+sub combineCycleByBase
+{
+	my $lane = shift @_;
+	my $cycle = shift @_;
+	my $base = shift @_;
+	$cycle = sprintf "%04d", $cycle;
+	my @cycleFiles = glob("$workDir/$lane" . "_$cycle" . "_*_$base" . ".jpg");
 	if($#cycleFiles > 2)
 	{
 		my $cycleString = "";
@@ -65,11 +102,23 @@ sub combine
 		{
 			my $tile = sprintf "%04d", (60 - $i);
 			my $nextTile =  sprintf "%04d", (61+$i);
-			$cycleString .= "$workDir/$lane" . "_$cycle" . "_$tile" . "_a.jpg $workDir/$lane" . "_$cycle" . "_$nextTile" . "_a.jpg ";	
+			$cycleString .= "$workDir/$lane" . "_$cycle" . "_$tile" . "_$base" . ".jpg $workDir/$lane" . "_$cycle" . "_$nextTile" . "_$base" . ".jpg ";	
 		}
-		my $montExec = "montage $cycleString -tile 2x60 -geometry +0+0 $workDir/montage_$lane" . "_$cycle" . ".jpg";
+		my $montExec = "montage $cycleString -tile 2x60 -geometry +0+0 $workDir/montage_$lane" . "_$cycle" . "_$base" . ".jpg";
 		print "$montExec\n";
 		system $montExec;
 	}
-	$semaphore->up();
 }
+
+sub combineLane
+{
+		my $lane = shift @_;
+		my @cycleFiles = glob("$workDir/montage_$lane" . "_????" . ".jpg");
+		my $cycleString = join(' ', sort(@cycleFiles));
+		my $numCycles = scalar @cycleFiles;
+		my $montExec = "montage $cycleString -tile $numCycles" . "x1 -geometry +0+0 $workDir/montage_$lane" . ".jpg";
+		print "$montExec\n";
+		system $montExec;
+		$semaphore->up();	
+}
+
