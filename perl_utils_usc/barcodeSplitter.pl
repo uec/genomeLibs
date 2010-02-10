392e55bfbb85785e724df1256055469516f103ce
zack <zack@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-02-10 21:21:59 +0000

    clarified, reduced size and complexity
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@310 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/barcodeSplitter.pl
+++ perl_utils_usc/barcodeSplitter.pl
@@ -1,58 +1,37 @@
 #!/usr/bin/perl
 use File::Basename;
 #####SET Consts
-$threeQualMatch = 25;
-$twoQualMatch = 25; 
+$perfectQualityCutoff = 25;
+$singleQualityErrorCutoff = 25;
 @availableBarcodes = ("ACG","CAT","GTA","TGC");
 ####################
 
-$threeQualMatch += 64;
-$twoQualMatch += 64; 
-
-$infile = $ARGV[0] || die "specify a file";
-open($infh, "<$infile") || die "couldnt open $infile";
-$infile = basename($infile);
+open($infh, $infile = $ARGV[0]) || die "couldnt open $infile";
 while(!eof($infh))
 {
-	$line[0] = readline($infh); $line[1] = readline($infh); $line[2] = readline($infh); $line[3] = readline($infh);
-	my $code = substr($line[1],0,3);
+        $line[0] = readline($infh); $line[1] = readline($infh); $line[2] = readline($infh); $line[3] = readline($infh);
+        my $code = substr($line[1],0,3);
         my $qual = substr($line[3],0,3);
-	my $failed = "unknown_barcodes";
-	$barcodeStats{$code}++;
+        my $resultFile = "unknown_barcodes";
+        my $resultLines = $line[0] . $line[1] . $line[2] . $line[3];
+        $barcodeStats{$code}++;
         for my $availableBarcode (@availableBarcodes)
         {
                 if ($h = &hammingdist($code, $availableBarcode) <= 1)
                 {
-                        if(!($qualityThreeMatch{$qual}))
+                        if(!($hasPerfectQuality{$qual}))
                         {
-                                my ($a,$b,$c) = split(//,$qual);
-                                $qualityThreeMatch{$qual} = ord($a) >= $threeQualMatch && ord($b) >= $threeQualMatch && ord($c) >= $threeQualMatch ? 1 : -1;
-                                $qualityTwoMatch{$qual} = ord($a) >= $twoQualMatch && ord($b) >= $twoQualMatch || ord($a) >= $twoQualMatch && ord($c) >= $twoQualMatch || ord($b) >= $twoQualMatch && ord($c) >= $twoQualMatch ? 1 : -1;
+                                $hasPerfectQuality{$qual} += (ord($_) < ($perfectQualityCutoff + 64) ? 0 : 1) for split(//,$qual);
+                                $hasSingleQualityError{$qual} += (ord($_) < ($singleQualityErrorCutoff + 64) ? 0 : 1) for split(//,$qual);
                         }
-                        if(($h == 0 && $qualityThreeMatch{$qual} == 1) || ($h == 1 && $qualityTwoMatch{$qual} == 1)) 
-			{
-				open($barcodes{$availableBarcode},">$availableBarcode" . "_$infile") if(!($barcodes{$availableBarcode}));
-				my $fh =  $barcodes{$availableBarcode} || die "couldn't write";
-				print $fh $line[0] . substr($line[1],5) . $line[2] . substr($line[3],5); 
-				undef $failed;
-			}
-                        else 
-			{
-				$failed = "lowQual_barcodes";
-			}
+                        $resultFile = (($h == 0 && $hasPerfectQuality{$qual} == length($qual)) || ($h == 1 && $hasSingleQualityError{$qual} >=  length($qual)-1)) ? $availableBarcode : "lowQual_barcodes";
+                        $resultLines = $line[0] . substr($line[1],5) . $line[2] . substr($line[3],5) unless $resultFile eq "lowQual_barcodes";
                 }
         }
-	if($failed)
-	{
-		open($barcodes{$failed},">$failed" . "_$infile") if(!($barcodes{$failed}));
-		my $fh =  $barcodes{$failed} || die "couldn't write";
-		print $fh $line[0] . $line[1] . $line[2] . $line[3]; 
-	}
+        open($resultFiles{$resultFile},">$resultFile" . "_" . basename($infile)) unless $resultFiles{$resultFile};
+        my $fh =  $resultFiles{$resultFile} || die "couldn't write";
+        print $fh $resultLines;
 }
-open($summary,">summary_$infile");
-foreach $key (sort {$barcodeStats{$b} <=> $barcodeStats{$a} } keys %barcodeStats)
-{
-     print $summary "$key $barcodeStats{$key}\n";
-}
-
+open($summary,">summary_" . basename($infile));
+print $summary "$_ $barcodeStats{$_}\n" for (sort {$barcodeStats{$b} <=> $barcodeStats{$a} } keys %barcodeStats);
 sub hammingdist{ length( $_[ 0 ] ) - ( ( $_[ 0 ] ^ $_[ 1 ] ) =~ tr[\0][\0] ) }
