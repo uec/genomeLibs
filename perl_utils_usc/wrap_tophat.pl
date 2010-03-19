5d99b8b1a499ec5cf5e6e2a27fe677d9c65add2e
zack <zack@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-03-19 21:45:39 +0000

    fixed renames and env var set
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@361 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/wrap_tophat.pl
+++ perl_utils_usc/wrap_tophat.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/perl
+$ENV{'BOWTIE_INDEXES'} = "/home/uec-00/shared/production/genomes/bowtie/";
 $execmd = join(" ", @ARGV);
 system($execmd);
 $file = scalar(@ARGV) > 3 ? $ARGV[$#ARGV - 1] : $ARGV[$#ARGV];
-system("mv coverage.wig " . $file. ".tophat_coverage.wig");
-system("mv accepted_hits.sam " . $file. ".tophat_hits.sam");
-system("mv junctions.bed " . $file. ".tophat_junctions.bed");
+system("mv tophat_out/coverage.wig " . $file. ".tophat_coverage.wig");
+system("mv tophat_out/accepted_hits.sam " . $file. ".tophat_hits.sam");
+system("mv tophat_out/junctions.bed " . $file. ".tophat_junctions.bed");
 
 
