67af2ce0a66a0deae46c3b13beace99c1e51a14b
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-07-21 19:03:56 +0000

    fixed some bugs, will now merge the sam's together
    but still seems to be having issues with samtools not liking sams
    
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@413 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/sam2bam.pl
+++ perl_utils_usc/sam2bam.pl
@@ -11,59 +11,74 @@ my $refFa = $ARGV[-1] || die "no input genome";
 my $refFai = $refFa;
 if( -e $refFa && $refFa =~ /\.fa$/)
 {
-		$refFai = $refFai . ".fai";
-		die "no fai found" unless -e $refFai;
+                $refFai = $refFai . ".fai";
+                die "no fai found" unless -e $refFai;
 }
 else
 {
-	die "no ref or ref index found";	
+        die "no ref or ref index found";
 }
-
 
 #handle multiple sams for a merge
 if(scalar(@ARGV) > 2)
 {
-	my $mergeFileString = join(" ", @ARGV[0,-2]);
-	my $mergedFileName = $samFn;
-	$mergedFileName = basename($mergedFileName);
-	$mergedFileName =~ s/\.sam$/\.merged\.sam/;
-	system("cat $mergeFileString > $mergedFileName");
-	$samFn = $mergedFileName;	
+        my @checkedSamFiles;
+        for my $samfile (@ARGV[0..($#ARGV - 1)])
+        {
+                if(-e $samfile)
+                {
+                        push @checkedSamFiles, $samfile;
+                }
+                else
+                {
+                        print STDERR "$samfile NOT FOUND!";
+                } 
+        }
+        my $mergeFileString = join(" ", @checkedSamFiles);
+        my $mergedFileName = $samFn;
+        $mergedFileName = basename($mergedFileName);
+        $mergedFileName =~ s/\.sam$/\.merged\.sam/;
+        print STDERR "cat $mergeFileString > $mergedFileName\n";
+        system("cat $mergeFileString > $mergedFileName");
+        $samFn = $mergedFileName;
 }
 
 
 my $finalProcId = 0;
 my $finalBamFn = 0;
 
-die "input must be map file" if ($samFn !~ /\.sam$/);
+die "input must be sam file" if ($samFn !~ /\.sam$/);
 my $samFnBase = basename($samFn);
 $samFnBase =~ s/\.sam$//;
 
 # Since we're going into a temp dir one level deeper, we have to adjust for relative FNs
 my $curIn = $samFn;
-my $curOut;
 
 
 
 # Sam to full bam
-$curIn = $curOut;
-$curOut = "${samFnBase}.bam";
+my $curOut = "${samFnBase}.bam";
+print STDERR "${SAMDIR}/samtools view -bt $refFai -o ${curOut} ${curIn}\n";
 system "${SAMDIR}/samtools view -bt $refFai -o ${curOut} ${curIn}";
+exit;
 
 # remove dups
 $curIn = $curOut;
 $curOut = "${samFnBase}.NODUPS.bam";
+print STDERR "${SAMDIR}/samtools rmdup -s ${curIn} ${curOut}\n";
 system "${SAMDIR}/samtools rmdup -s ${curIn} ${curOut}";
 
 # Sort bam
 $curIn = $curOut;
 $curOut = "${samFnBase}.NODUPS.sorted.bam";
 my $curOutPrefix = $curOut; $curOutPrefix =~ s/\.bam$//g; # You only specify the prefix
+print STDERR "${SAMDIR}/samtools sort ${curIn} ${curOutPrefix}\n";
 system "${SAMDIR}/samtools sort ${curIn} ${curOutPrefix}";
 
 # calmd
 $curIn = $curOut;
 $curOut = "${samFnBase}.NODUPS.sorted.calmd.bam";
+print STDERR "${SAMDIR}/samtools calmd -b ${curIn} ${refFa} > ${curOut}\n";
 system "${SAMDIR}/samtools calmd -b ${curIn} ${refFa} > ${curOut}";
 
 # Set final BAM name
@@ -72,4 +87,5 @@ $finalBamFn = $curOut;
 # Index bam
 $curIn = $curOut;
 $curOut = "${samFnBase}.NODUPS.sorted.calmd.bam.bai";
+print STDERR "${SAMDIR}/samtools index ${curIn} ${curOut}\n";
 system "${SAMDIR}/samtools index ${curIn} ${curOut}";
