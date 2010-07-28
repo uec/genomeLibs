b7c617d75388ef46e5d56569347916081675074c
benberm <benberm@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-07-28 00:15:44 +0000

    Removed chrom20/21 filtering.
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@433 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/generateChromBamsToMethylChromTables.pl
+++ perl_utils_usc/generateChromBamsToMethylChromTables.pl
@@ -9,7 +9,7 @@ my $USAGE = "generateChromBamsToMethylChromTables.pl [--noremoveTemps] [--useRea
 my @regions = ("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", 
 	       "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", 
 	       "chr19", "chr20", "chr21", "chr22", "chrX", "chrY", "chrM");
-@regions = ("chr20","chr21");
+#@regions = ("chr20","chr21");
 
 my $SAMDIR = "/home/uec-00/bberman/bin";
 my $RMTMPS = 0;
