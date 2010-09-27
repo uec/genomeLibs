d02e11d66d7bcdc9d44bff7bc9fbbf8b81fea4ab
benberm <benberm@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-09-27 21:18:01 +0000

    first checked in.  simple copy of split_gff_by_src.pl
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@491 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

---
+++ perl_utils_usc/split_gff_by_feat_type.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Basename;
+
+my @files = @ARGV;
+
+
+foreach my $f (@files)
+{
+
+    my ($name, $path, $suf) = fileparse($f, qr/\.[^.]*/);
+    print "($name)\t($path)\t($suf)\n";
+
+    # Keep output strings in a hash
+    my $src_h = {};
+
+    die "Couldn't read $f\n" unless (open(F,$f));
+    my $on_line = 1;
+    while (my $line = <F>)
+    {
+	if ($line =~ /^\s*\#/ || $line =~ /^\s*$/ || $line =~ /track name/)
+	{
+	}
+	else
+	{
+	    my @flds = split(/\t/,$line);
+	    my $src = @flds[2];
+	    $src =~ s/[\/]//g;
+	    $src =~ s/\./-/g;
+	    $src_h->{$src} .= $line;
+	}
+	
+	print STDERR "On line $on_line\n" if (($on_line%10000)==0);
+ 	$on_line++;
+    }
+    
+    # Close input file
+    close(F);
+
+    # And write output files
+    SRC: foreach my $src (keys(%$src_h))
+    {
+	next SRC if (!$src || ($src =~ /^\s*\#/) || ($src =~ /^\s*$/));
+
+	my $str = $src_h->{$src};
+	$src =~ s/\/\#\://g;
+	my $outfn = $path.$name."_$src".$suf;
+	print "Writing to $outfn\n";
+
+	if (1)
+	{
+	    die "Can't write to $outfn\n" unless (open(OUT,">$outfn"));
+	    print OUT $str;
+	    close(OUT);
+	}
+    }
+}
