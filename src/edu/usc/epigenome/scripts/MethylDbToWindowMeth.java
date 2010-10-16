72bbff5229e738d3f95d44af367615907a0d81ae
benberm <benberm@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-10-16 22:07:45 +0000

    Seems to be working.  Still needs a header line and more descriptive output filename.
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@523 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- src/edu/usc/epigenome/scripts/MethylDbToWindowMeth.java
+++ src/edu/usc/epigenome/scripts/MethylDbToWindowMeth.java
@@ -26,6 +26,7 @@ import com.mallardsoft.tuple.Tuple;
 
 
 import edu.usc.epigenome.genomeLibs.ListUtils;
+import edu.usc.epigenome.genomeLibs.MatUtils;
 import edu.usc.epigenome.genomeLibs.FeatAligners.FeatAligner;
 import edu.usc.epigenome.genomeLibs.FeatAligners.FeatAlignerEachfeat;
 import edu.usc.epigenome.genomeLibs.GenomicRange.GenomicRangeWithRefpoint;
@@ -33,6 +34,10 @@ import edu.usc.epigenome.genomeLibs.MethylDb.Cpg;
 import edu.usc.epigenome.genomeLibs.MethylDb.CpgIteratorMultisample;
 import edu.usc.epigenome.genomeLibs.MethylDb.MethylDbQuerier;
 import edu.usc.epigenome.genomeLibs.MethylDb.MethylDbUtils;
+import edu.usc.epigenome.genomeLibs.MethylDb.CpgSummarizers.CpgMethLevelSummarizer;
+import edu.usc.epigenome.genomeLibs.MethylDb.CpgSummarizers.CpgSummarizer;
+import edu.usc.epigenome.genomeLibs.MethylDb.CpgWalker.CpgWalker;
+import edu.usc.epigenome.genomeLibs.MethylDb.CpgWalker.CpgWalkerParams;
 
 
 public class MethylDbToWindowMeth {
@@ -41,10 +46,10 @@ public class MethylDbToWindowMeth {
 	
 	@Option(name="-maxFeatSize",usage="maximum size of features to include (default Inf)")
     protected int maxFeatSize = Integer.MAX_VALUE;
-	@Option(name="-useSpatialCpgWeighting",usage="If set, weights a CpG by the spatial distance it covers, otherwise weight all CpGs equally (default true)")
-    protected boolean useSpatialCpgWeighting = true;
+	@Option(name="-useSpatialCpgWeighting",usage="If set, weights a CpG by the spatial distance it covers, otherwise weight all CpGs equally (default false)")
+    protected boolean useSpatialCpgWeighting = false;
 	@Option(name="-windSize",usage="add one or more window sizes to be tested in addition to the domain of the feature itself")
-    protected List<Integer> windSize = new ArrayList<Integer>();
+    protected List<Integer> windSizes = new ArrayList<Integer>();
     @Option(name="-outputPrefix",usage="Prefix for output files (default methylDb)")
     protected String outputPrefix = "methylDb";
     @Option(name="-minCTreads",usage="Minimum number of C or T reads to count as a methylation value")
@@ -52,7 +57,7 @@ public class MethylDbToWindowMeth {
     @Option(name="-maxOppStrandAfrac",usage="As on the opposite strand are evidence for mutation or SNP. " +
     		"This sets a maximum number of observed As on the opposite strand (default 0.1)")
     protected double maxOppStrandAfrac = 0.1;
-	@Option(name="-featFilter",usage="We will take the intersection with this feature. Must be a featType in the features table")
+	@Option(name="-featFilter",usage="We will take the intersection with this feature (only for -windSize windows, NOT for primary feats). Must be a featType in the features table")
 	protected List<String> featFilters = new ArrayList<String>(5);
 	// receives other command line parameters than options
 	@Argument
@@ -118,6 +123,8 @@ public class MethylDbToWindowMeth {
 			return;
 		}
 		
+		windSizes.add(0, new Integer(0));
+
 		
 		int nS = tablePrefixes.size();
 		int nFeatTypes = featFns.size();
@@ -143,21 +150,29 @@ public class MethylDbToWindowMeth {
 
 			for (String chrStr : MethylDbUtils.CHROMS) // Arrays.asList("chr11")) //
 			{
-				processChrom(chrStr, feats, tablePrefixes);
+				processChrom(chrStr, feats, tablePrefixes, writer);
 			}
 
 
 		}
 		writer.close();
 	}
-	
-	
-	protected void processChrom(String chrStr, ChromFeatures feats, List<String> tablePrefixes)
+
+
+	protected void processChrom(String chrStr, ChromFeatures feats, List<String> tablePrefixes, PrintWriter writer)
 	throws Exception
 	{
 		// Setup the DB queries
 		int chr = feats.chrom_from_public_str(chrStr); 
 		int nS = tablePrefixes.size();
+		
+		// Make a CpgMethLevelSummarizer for each table
+		CpgMethLevelSummarizer[] summarizers = new CpgMethLevelSummarizer[nS];
+		for (int i =0; i<nS; i++)
+		{
+			summarizers[i] = new CpgMethLevelSummarizer();
+		}
+	
 
 		Iterator featit = feats.featureIterator(chr);
 		System.err.println("Processing " + chrStr);
@@ -165,54 +180,106 @@ public class MethylDbToWindowMeth {
 		FEAT: while (featit.hasNext())
 		{
 			featNum++;
-			
+
 			SimpleGFFRecord rec = (SimpleGFFRecord)featit.next();
 			StrandedFeature.Strand featStrand = rec.getStrand();
 			String featName = null; // rec.getSeqName();
 			int featS = rec.getStart();
 			int featE = rec.getEnd();
-			
-			try 
+
+			for (Integer windSize : windSizes)
 			{
 
-				
-				// Meth
-				MethylDbQuerier params = new MethylDbQuerier();
-				for (String featFilter : this.featFilters)
+				try 
 				{
-					params.addFeatFilter(featFilter,0);
-				}
-				params.setMinCTreads(this.minCTreads);
-				params.setMaxOppstrandAfrac(this.maxOppStrandAfrac);
-				params.addRangeFilter(chrStr,featS,featE);
-				
-				CpgIteratorMultisample cpgit = new CpgIteratorMultisample(params, tablePrefixes);
 
-				
-				
-				while (cpgit.hasNext()) 
+					int featS_w = featS;
+					int featE_w = featE;
+					if (windSize>0)
+					{
+						double windFlank = (double)windSize / 2.0;
+						double windC = Math.round(MatUtils.mean(featS, featE));
+						featS_w = (int)Math.round(windC - windFlank);
+						featE_w = (int)Math.round(windC + windFlank);
+					}
+					
+					
+
+
+					// Meth
+					MethylDbQuerier params = new MethylDbQuerier();
+
+					// Only use features for window size > 0
+					if (windSize>0)
+					{
+						for (String featFilter : this.featFilters)
+						{
+							params.addFeatFilter(featFilter,0);
+						}
+					}
+					params.setMinCTreads(this.minCTreads);
+					params.setMaxOppstrandAfrac(this.maxOppStrandAfrac);
+					params.addRangeFilter(chrStr,featS_w,featE_w);
+
+					CpgIteratorMultisample cpgit = new CpgIteratorMultisample(params, tablePrefixes);
+					
+					// Reset summarizers
+					for (int i =0; i<nS; i++)
+					{
+						summarizers[i].reset();
+					}
+
+
+					int nCpgs = 0;
+					while (cpgit.hasNext()) 
+					{
+						Cpg[] cpgs = cpgit.next();
+
+						int chromPos = cpgs[0].chromPos;
+						StrandedFeature.Strand cpgStrand = cpgs[0].getStrand();
+
+
+						for (int i=0; i<nS; i++)
+						{
+							summarizers[i].streamCpg(cpgs[i]);
+						}
+						
+						//						System.err.println(String.format("\tON CPG %s", cpgs[0].toString()));
+						nCpgs++;
+					}
+
+
+					if ((featNum % 100)==0)
+					{
+						System.err.print(String.format(
+								"ON WINDSIZE=%d, FEATURE %d coords: chr=%s\tfeatS=%d\tfeatE=%d\tflankS=%d\tflankE=%d\tfeatStrand=%s\n\t%d Cpgs\n",
+								windSize, featNum, chrStr, featS, featE, featS_w, featE_w, ""+featStrand, nCpgs));
+					}
+					
+					for (int i =0; i<nS; i++)
+					{
+						writer.printf("%.2f,", summarizers[i].getValMean(this.useSpatialCpgWeighting));
+					}
+
+
+				}
+				catch (Exception e)
 				{
-					Cpg[] cpgs = cpgit.next();
+					Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe(String.format(
+							"PROBLEM WITH FEATURE %d coords: chr=%s\tfeatS=%d\tfeatE=%d\tfeatStrand=%s\n%s\n",
+							featNum, chrStr, featS, featE, ""+featStrand,e.toString()));
+					e.printStackTrace();
+					System.exit(1);
 
-					int chromPos = cpgs[0].chromPos;
-					StrandedFeature.Strand cpgStrand = cpgs[0].getStrand();
 				}
-
 
 			}
-			catch (Exception e)
-			{
-				Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe(String.format(
-						"PROBLEM WITH FEATURE %d coords: chr=%s\tfeatS=%d\tfeatE=%d\tfeatStrand=%s\n%s\n",
-						featNum, chrStr, featS, featE, ""+featStrand,e.toString()));
-				e.printStackTrace();
-				System.exit(1);
-
-			}
-
-
 			// Increment feat ind
 			this.fCurFeatInd++;
+			
+			// New line
+			writer.println();
+
 		}
 
 		
