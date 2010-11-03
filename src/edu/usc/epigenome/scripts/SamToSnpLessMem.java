4bb44d89a7a102b7cffe9f3e7f9c844c7a1d77d0
dnaase <dnaase@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-11-03 01:21:08 +0000

    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@540 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

---
+++ src/edu/usc/epigenome/scripts/SamToSnpLessMem.java
@@ -0,0 +1,291 @@
+package edu.usc.epigenome.scripts;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMRecord;
+import net.sf.samtools.util.CloseableIterator;
+import edu.usc.epigenome.genomeLibs.PicardUtils;
+import edu.usc.epigenome.genomeLibs.MethylDb.MethylDbUtils;
+
+public class SamToSnpLessMem {
+
+	/**
+	 * @param args
+	 */
+	final private static String prefix = "methylCGsRich_ASM_";
+	final private static String USAGE = "SamToSnp [opts] sampleName file1.bam file2.bam ...";
+
+	final private static int PURGE_INTERVAL = 20000; // We purge our stored Cpgs once we get this many bases past them.
+	
+	
+	/**
+	 * object vars
+	 */
+
+	
+	/**
+	 * @param args
+	 */
+	@Option(name="-chrom",multiValued=true,usage="One or more chroms, eg. --chrom chr1 --chrom chr5")
+	protected List<String> chrs = new ArrayList<String>(25);
+	@Option(name="-minConv",usage="minimum number of converted cytosines required")
+	protected int minConv = 0;
+	@Option(name="-outputHcphs",usage=" Output HCpH cytosines (can't use more than 1 input file)")
+	protected boolean outputHcphs = false;
+	@Option(name="-minMapQ",usage="minimum mapping quality (default 30)")
+	protected int minMapQ = 30;
+	@Option(name="-minBaseQual",usage="minimum Base quality (default 30)")
+	protected int minBaseQual = 30;
+	@Option(name="-minAlleleCount",usage="minimum Allele Count (default 3)")
+	protected static int minAlleleCount = 3;
+	@Option(name="-minAlleleFreq",usage="minimum BAllele Frequency (default 0.3)")
+	protected static double minAlleleFreq = 0.30;
+	@Option(name="-debug",usage=" Debugging statements (default false)")
+	protected boolean debug = false;
+
+		// receives other command line parameters than options
+	@Argument
+	private List<String> stringArgs = new ArrayList<String>();
+
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	throws Exception
+	{
+		new SamToSnpLessMem().doMain(args);
+	}
+
+	public void doMain(String[] args)
+	throws Exception {
+
+		CmdLineParser parser = new CmdLineParser(this);
+
+		parser.setUsageWidth(80);
+		try
+		{
+			parser.parseArgument(args);
+			if (stringArgs.size() < 2) throw new CmdLineException(USAGE);
+			
+			
+		}
+		catch (CmdLineException e)
+		{
+			System.err.println(e.getMessage());
+
+			parser.printUsage(System.err);
+			System.err.println();
+			return;
+		}
+
+		String sampleName = stringArgs.remove(0);
+		if (chrs.size()==0) chrs = MethylDbUtils.CHROMS;
+		for (final String chr : chrs)
+		{
+			String tableName = prefix + sampleName + "_" + chr;
+
+			for (final String fn : stringArgs)
+			{
+				File inputSamOrBamFile = new File(fn);
+
+				final SAMFileReader inputSam = new SAMFileReader(inputSamOrBamFile);
+				inputSam.setValidationStringency(SAMFileReader.ValidationStringency.SILENT);
+				
+				
+				
+				
+				// We can only purge if we are the only input file and we are sorted.
+				boolean canPurge = ((inputSam.hasIndex()) && (stringArgs.size() == 1));
+				//boolean canPurge = false;
+				Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).info(String.format("Able to purge at interval %d? %s\n", 
+						PURGE_INTERVAL, (canPurge) ? "Yes" : "Purging not available - either unsorted BAM or multiple BAMs for the same chrom"));
+				
+				TreeMap<Integer,Integer[]> allelePosition = allelePos( inputSam, canPurge, chr );
+
+				outputSNP(allelePosition, tableName);
+				System.err.println("finished!");
+			}
+		}
+	}
+
+		protected TreeMap<Integer,Integer[]> allelePos( SAMFileReader inputSam, boolean canPurge, String chr)
+		throws Exception{
+			CloseableIterator<SAMRecord> chrItForAllele = inputSam.query(chr, 0, 0, false);
+			int lastBaseSeen = 0;
+			int lastPurge = 0;
+			int recCounter = 0;
+			
+			TreeMap<Integer, Integer> readsDepth = new TreeMap<Integer, Integer>();
+			TreeMap<Integer,Integer[]> allelePosition = new TreeMap<Integer,Integer[]>();
+			
+			recordSub: while (chrItForAllele.hasNext())
+			{
+				if (canPurge && (lastBaseSeen > (lastPurge+PURGE_INTERVAL)))
+				{
+					
+					lastPurge = lastBaseSeen;
+				}
+				
+				SAMRecord samRecord = chrItForAllele.next();
+
+				// Filter low qual
+				int mapQual = samRecord.getMappingQuality();
+				byte[] baseQual = samRecord.getBaseQualities();
+				boolean unmapped = samRecord.getReadUnmappedFlag();
+				if (unmapped || (mapQual < minMapQ))
+				{
+					continue recordSub;
+				}
+
+
+				String seq = PicardUtils.getReadString(samRecord, true);
+				
+				recCounter++;
+				if ((recCounter % 1E5)==0)
+				{
+					System.err.printf("On new record #%d\n",recCounter);
+					if (canPurge) System.gc();
+				}
+
+
+				try
+				{
+					String ref = PicardUtils.refStr(samRecord, true);
+
+					if (seq.length() != ref.length())
+					{
+						System.err.println("SeqLen(" + seq.length() + ") != RefLen(" + ref.length() + ")");
+						System.err.println(seq + "\n" + ref);
+					}
+					//System.err.println(seq + "\n" + ref);
+
+					boolean negStrand = samRecord.getReadNegativeStrandFlag();
+					int alignmentS = samRecord.getAlignmentStart();
+					int	onRefCoord = (negStrand) ? samRecord.getUnclippedEnd() : alignmentS; 
+
+					if ((recCounter % 1E5)==0 && onRefCoord-10000 > 0)
+					{
+						System.err.printf("On new onRefCoord #%d\n",onRefCoord-10000);
+						readsDepth.headMap(onRefCoord-10000).clear();
+						//System.err.printf("On new onRefCoord #%d\n",readsDepth.size());
+						//Iterator<Integer> cleanKey = readsDepth.headMap(onRefCoord-10000).keySet().iterator();
+						//while(cleanKey.hasNext()){
+							//readsDepth.remove(cleanKey.next());
+						//}
+					}
+					
+					if ((this.outputHcphs) && (alignmentS < lastBaseSeen))
+					{
+						System.err.printf("BAM must be ordered in order to handle -outputCphs: %d<%d\n",alignmentS, lastBaseSeen);
+						System.exit(1);
+					}
+					lastBaseSeen = alignmentS;
+
+					int seqLen = Math.min(seq.length(), ref.length());
+					
+					for (int i = 0; i < seqLen; i++){
+						char refi = ref.charAt(i);
+
+						byte baseQS = (negStrand) ? baseQual[seqLen-1-i] : baseQual[i];	
+						
+						if(baseQS >= minBaseQual && (PicardUtils.isAdenine(i,seq) || PicardUtils.isGuanine(i,seq))){
+							if(allelePosition.containsKey(onRefCoord)){
+								Integer[] tempInt = allelePosition.get(onRefCoord);
+									tempInt[1]++;//  total reads number
+
+									
+								if ((PicardUtils.isGuanine(i,ref) && PicardUtils.isAdenine(i,seq)) || (PicardUtils.isAdenine(i,ref) && PicardUtils.isGuanine(i,seq))){	
+									tempInt[0]++;// allele reads number
+								}
+								allelePosition.put(onRefCoord,tempInt);
+
+									
+							}
+							else{
+								Integer[] tempInt = new Integer[2];
+									tempInt[1] = readsDepth.get(onRefCoord);
+									if(tempInt[1] != null)
+										tempInt[1]++;
+									else
+										tempInt[1] = 1;
+									readsDepth.put(onRefCoord,tempInt[1]);
+
+								if ((PicardUtils.isGuanine(i,ref) && PicardUtils.isAdenine(i,seq)) || (PicardUtils.isAdenine(i,ref) && PicardUtils.isGuanine(i,seq))){	
+									tempInt[0] = 1;
+
+									allelePosition.put(onRefCoord,tempInt);
+								}
+							}
+						}
+						if (refi == '-')
+						{
+							// It's a deletion in reference, don't advance
+						}
+						else
+						{
+							int inc = (negStrand) ? -1 : 1;
+							onRefCoord += inc;
+						}
+					}
+				}
+				catch (Exception f)
+				{
+					System.err.println("-----------------------------------------");
+					System.err.println("Couldn't handle seq #" + recCounter);
+					System.err.println(seq);
+					f.printStackTrace(System.err);
+					System.err.println("-----------------------------------------");
+				}
+			}
+			chrItForAllele.close();
+			return allelePosition;
+			
+		}		
+
+		protected void outputSNP(TreeMap<Integer,Integer[]> allelePosition, String tableName)
+		throws FileNotFoundException{
+			Iterator<Integer> it = allelePosition.keySet().iterator();
+			System.err.println("-----------------------------------------");
+			String fn = tableName + "_SNP_afterBaseQfilter" + ".txt";
+			PrintWriter writer = new PrintWriter(new File(fn));
+			while(it.hasNext()){	
+				Integer snp = it.next();
+				Integer[] tempInt = allelePosition.get(snp);
+				 double tempDouble1 = tempInt[0];
+				 double tempDouble2 = tempInt[1];
+				 double freq1 = tempDouble1/tempDouble2;
+				 double freq2 = (tempDouble2-tempDouble1)/tempDouble2;
+
+				 if(tempInt[1] <= 10 && (tempInt[0] < minAlleleCount || (tempInt[1] - tempInt[0]) < minAlleleCount) ){
+
+					 continue;
+				 }
+				 else if(tempInt[1] > 10 && (freq1 < minAlleleFreq || freq2 < minAlleleFreq)){
+					 continue;
+				 }
+				 else{
+					 writer.printf("%d\t%d\t%d\n",snp,tempInt[1],tempInt[0]);
+					 
+				 }
+				
+			}
+			writer.close();
+		}
+
+}
+
