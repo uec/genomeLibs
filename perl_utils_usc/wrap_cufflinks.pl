cd06a25e3283a1b31aa0997f192f506b3cc373fe
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2010-12-15 02:21:02 +0000

    cufflinks pbs wrapper
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@546 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

---
+++ perl_utils_usc/wrap_cufflinks.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use File::Basename;
+
+$ENV{'BOWTIE_INDEXES'} = "/home/uec-00/shared/production/genomes/bowtie/";
+$ENV{'PATH'} .= "/home/uec-00/shared/production/software/cufflinks/default:/home/uec-00/shared/production/software/tophat/default:/home/uec-00/shared/production/software/bowtie/default";
+$execmd = "/home/uec-00/shared/production/software/cufflinks/default/cufflinks " . join(" ", @ARGV);
+print "$execmd\n";
+system($execmd);
+$file = $ARGV[$#ARGV];
+$file = basename($file);
+system("mv transcripts.gtf " . $file. ".cufflinks_transcripts.gtf");
+system("mv transcripts.expr " . $file. ".cufflinks_transcripts.expr");
+system("mv genes.expr " . $file. ".cufflinks_genes.expr");
