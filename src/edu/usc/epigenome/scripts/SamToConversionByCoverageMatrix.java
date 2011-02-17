54f5bc32aa90d15323e53e8966b1f3524c4bd7fa
benberm <benberm@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2011-02-17 00:42:13 +0000

    First checked in.
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@578 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

---
+++ src/edu/usc/epigenome/scripts/SamToConversionByCoverageMatrix.java
@@ -0,0 +1,511 @@
+package edu.usc.epigenome.scripts;
+
+import net.sf.samtools.*;
+import net.sf.samtools.util.CloseableIterator;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.usckeck.genome.MatUtils;
+
+import edu.usc.epigenome.genomeLibs.PicardUtils;
+import edu.usc.epigenome.genomeLibs.MethylDb.Cpg;
+import edu.usc.epigenome.genomeLibs.MethylDb.MethylDbUtils;
+
+
+public class SamToConversionByCoverageMatrix {
+
+	final private static String prefix = "conversionByCoverage_";
+	final private static String USAGE = "SamToConversionByCoverageMatrix [opts] sampleName file1.bam file2.bam ...";
+
+//	final private static int PURGE_INTERVAL = 100000; // We purge our stored Cpgs once we get this many bases past them.
+	final private static int PURGE_INTERVAL = 20000; // We purge our stored Cpgs once we get this many bases past them.
+	
+	
+	/**
+	 * object vars
+	 */
+
+	
+	/**
+	 * @param args
+	 */
+	@Option(name="-chrom",multiValued=true,usage="One or more chroms, eg. --chrom chr1 --chrom chr5")
+	protected List<String> chrs = new ArrayList<String>(25);
+	@Option(name="-minConv",usage="minimum number of converted cytosines required")
+	protected int minConv = 1;
+	@Option(name="-useCpgsToFilter",usage=" Use CpGs and CpHs to filter if true, otherwise just CpHs (default false)")
+	protected boolean useCpgsToFilter = false;
+	@Option(name="-outputCphs",usage=" Output CpH cytosines (default false)")
+	protected boolean outputCphs = false;
+	@Option(name="-minMapQ",usage="minimum mapping quality (default 0)")
+	protected int minMapQ = 0;
+	@Option(name="-maxCoverageOutput",usage="maximum coverage for output array (default 100)")
+	protected int maxCoverageOutput = 100;
+	@Option(name="-minOppStrandCoverage",usage="minimum coverage of opposite strand to confirm cytosine (default 5)")
+	protected int minOppStrandCoverage = 5;
+	@Option(name="-minNextBaseCoverage",usage="minimum coverage of next base to distinguish CpG/CpH (default 5)")
+	protected int minNextBaseCoverage = 5;
+	@Option(name="-debug",usage=" Debugging statements (default false)")
+	protected boolean debug = false;
+	@Option(name="-maxOppStrandAfrac",usage="As on the opposite strand are evidence for mutation or SNP. " +
+	"This sets a maximum number of observed As on the opposite strand (default 0.1)")
+	protected double maxOppStrandAfrac = 0.101;
+	@Option(name="-maxNextNonGfrac",usage="If the base following the C has more than this ratio of non-G bases, we don't count it. (default 0.1)")
+	protected double maxNextNonGfrac = 0.101;
+	
+	// receives other command line parameters than options
+	@Argument
+	private List<String> stringArgs = new ArrayList<String>();
+
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	throws Exception
+	{
+		new SamToConversionByCoverageMatrix().doMain(args);
+	}
+
+	public void doMain(String[] args)
+	throws Exception {
+
+		CmdLineParser parser = new CmdLineParser(this);
+		// if you have a wider console, you could increase the value;
+		// here 80 is also the default
+		parser.setUsageWidth(80);
+		try
+		{
+			parser.parseArgument(args);
+			if (stringArgs.size() < 2) throw new CmdLineException(USAGE);
+			
+			if (this.outputCphs && (stringArgs.size()>2)) throw new CmdLineException("Can't use -outputCphs with multiple input bam files");
+		}
+		catch (CmdLineException e)
+		{
+			System.err.println(e.getMessage());
+			// print the list of available options
+			parser.printUsage(System.err);
+			System.err.println();
+			return;
+		}
+
+		String sampleName = stringArgs.remove(0);
+
+		int recCounter = 0;
+		int usedCounter = 0;
+		int filteredOutCounter = 0;
+
+		// Cph counters
+		int numCphConvertedWithFilt = 0;
+		int numCphTotalWithFilt = 0;
+		int numCphConvertedNoFilt = 0;
+		int numCphTotalNoFilt = 0;
+		
+
+		// Iterate through chroms
+		if (chrs.size()==0) chrs = MethylDbUtils.CHROMS;
+		for (final String chr : chrs)
+		{
+			// Output map
+			int[][] cphCounts = new int[maxCoverageOutput][maxCoverageOutput]; 
+			int[][] cpgCounts = new int[maxCoverageOutput][maxCoverageOutput]; 
+
+			// Current cytosines
+			SortedMap<Integer,Cpg> cytosines = new TreeMap<Integer,Cpg>();
+			
+			for (final String fn : stringArgs)
+			{
+				File inputSamOrBamFile = new File(fn);
+
+				final SAMFileReader inputSam = new SAMFileReader(inputSamOrBamFile);
+				inputSam.setValidationStringency(SAMFileReader.ValidationStringency.SILENT);
+				
+				int querys = 7000000;
+				int querye = 9000000;
+				querys = 0;
+				querye = 0;
+				CloseableIterator<SAMRecord> chrIt = inputSam.query(chr, querys, querye, false);
+				
+				// We can only purge if we are the only input file and we are sorted.
+				boolean canPurge = ((inputSam.hasIndex()) && (stringArgs.size() == 1));
+				Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).info(String.format("Able to purge at interval %d? %s\n", 
+						PURGE_INTERVAL, (canPurge) ? "Yes" : "Purging not available - either unsorted BAM or multiple BAMs for the same chrom"));
+				int lastBaseSeen = 0;
+				int lastPurge = 0;
+				record: while (chrIt.hasNext())
+				{
+					if (canPurge && (lastBaseSeen > (lastPurge+PURGE_INTERVAL)))
+					{
+						// First, increment counters
+						if (cytosines != null) incrementCytosineCounters(cytosines.headMap(new Integer(lastPurge)), cpgCounts, cphCounts);
+						//System.err.printf("Purging %d cs from %d-%d\n", cytosines.headMap(new Integer(lastPurge)).size(), lastPurge, lastBaseSeen);
+
+						
+						// Weird, if i just set cytosines to be the tailMap (as in 1, below) garbage collection doesn't actually clean up
+						// the old part (just backed by the original data structure). So i actually have to copy it to a new map. (as in 2)
+						//(1) cytosines = cytosines.tailMap(new Integer(lastPurge));
+						cytosines = new TreeMap<Integer,Cpg>(cytosines.tailMap(new Integer(lastPurge))); // (2)
+						
+						lastPurge = lastBaseSeen;
+					}
+					
+					SAMRecord samRecord = chrIt.next();
+
+					// Filter low qual
+					int mapQual = samRecord.getMappingQuality();
+					boolean unmapped = samRecord.getReadUnmappedFlag();
+					if (unmapped || (mapQual < minMapQ))
+					{
+						continue record;
+					}
+
+
+					String seq = PicardUtils.getReadString(samRecord, true);
+
+					recCounter++;
+					if ((recCounter % 1E5)==0)
+					{
+						System.err.printf("On new record #%d, lastBase=%d, purged tree size:%d\n",recCounter,lastBaseSeen, cytosines.size());
+						if (canPurge) System.gc();
+					}
+
+
+					try
+					{
+						String ref = PicardUtils.refStr(samRecord, true);
+
+						if (seq.length() != ref.length())
+						{
+							System.err.println("SeqLen(" + seq.length() + ") != RefLen(" + ref.length() + ")");
+							System.err.println(seq + "\n" + ref);
+						}
+						//System.err.println(seq + "\n" + ref);
+
+						boolean negStrand = samRecord.getReadNegativeStrandFlag();
+						int alignmentS = samRecord.getAlignmentStart();
+						int	onRefCoord = (negStrand) ? samRecord.getUnclippedEnd() : alignmentS; 
+
+						if ((this.outputCphs) && (alignmentS < lastBaseSeen))
+						{
+							System.err.printf("BAM must be ordered in order to handle -outputCphs: %d<%d\n",alignmentS, lastBaseSeen);
+							System.exit(1);
+						}
+						lastBaseSeen = alignmentS;
+							
+							
+
+						int numConverted = 0;
+						int convStart = Integer.MAX_VALUE;
+						int seqLen = Math.min(seq.length(), ref.length());
+						for (int i = 0; i < seqLen; i++)
+						{
+							char refi = ref.charAt(i);
+							char seqi = seq.charAt(i);
+							char nextBaseRef = PicardUtils.nextBaseRef(i, ref);
+							char nextBaseSeq = PicardUtils.nextBaseSeq(i, seq);
+
+							if ((i < (seqLen-1)) && PicardUtils.isCytosine(i,ref,false) && PicardUtils.isCytosine(i, seq,true)) // The last one is too tricky to deal with since we don't know context
+							{
+								boolean iscpg = PicardUtils.isCpg(i,ref);
+								boolean conv = PicardUtils.isConverted(i,ref,seq);
+								
+								
+
+								if (conv && (this.useCpgsToFilter || !iscpg)) numConverted++;
+
+								// If this is the first legal one , note it
+								if ((convStart==Integer.MAX_VALUE) && (numConverted>=this.minConv) )
+								{
+									convStart = i;
+								}
+
+
+								if (iscpg || this.outputCphs)
+								{
+									if (i<convStart)
+									{
+										// In the non-conversion filter zone
+										filteredOutCounter++;
+										//System.err.printf("Rec %d\tpos=%d\n",recCounter,i);
+									}
+									else
+									{
+										// Past the non-conversion filter, use it
+										usedCounter++;
+									}
+
+									Cpg cpg = findOrCreateCpg(cytosines, onRefCoord, negStrand, nextBaseRef);
+									// See if we can fix the context for this CpG
+									if (cpg.getNextBaseRef() == '0') cpg.setNextBaseRef(nextBaseRef);
+									
+									this.incrementCpg(cpg, seqi, i<convStart, nextBaseSeq);
+								}
+								
+								// Count CpHs as a byproduct
+								if (!iscpg)
+								{
+									numCphTotalNoFilt++;
+									if (conv) numCphConvertedNoFilt++;
+									
+									if (i>=convStart)
+									{
+										numCphTotalWithFilt++;
+										if (conv) numCphConvertedWithFilt++;
+									}
+								}
+
+
+
+							} // IsCytosine
+
+//							boolean oppositeCpg = PicardUtils.isOppositeCpg(i,ref);
+//							if (oppositeCpg)
+							// Since we are doing CpHs, we need opp strand info for all guanines
+							if (PicardUtils.isGuanine(i, ref))
+							{
+								// Look for cpg on opposite strand
+								// Be careful, the "nextBaseRef" is now on the opposite strand!!
+								char nextBaseRefRev = PicardUtils.nextBaseRef(i, ref, true);
+								Cpg cpg = findOrCreateCpg(cytosines, onRefCoord, !negStrand, nextBaseRefRev);
+								
+								this.incrementOppositeCpg(cpg, seqi);
+							}
+
+
+							// Increment genomic coord position
+							if (refi == '-')
+							{
+								// It's a deletion in reference, don't advance
+							}
+							else
+							{
+								int inc = (negStrand) ? -1 : 1;
+								onRefCoord += inc;
+							}
+
+						} // i (pos within read)
+					}
+					catch (Exception e)
+					{
+						System.err.println("-----------------------------------------");
+						System.err.println("Couldn't handle seq #" + recCounter);
+						System.err.println(seq);
+						e.printStackTrace(System.err);
+						System.err.println("-----------------------------------------");
+//						chrIt.close();
+//						System.exit(1);
+					}
+
+				} // record
+
+				chrIt.close();
+				inputSam.close();
+
+			}
+
+			// Take care of last cytosines
+			if (cytosines != null) incrementCytosineCounters(cytosines, cpgCounts, cphCounts);
+
+			// And output the chrom
+			String fn = String.format("%s%s-%s-minOppCvg%d-minNextCvg%d-maxOppA%.3f-maxNextNonG%.3f-%s.csv", 
+					prefix,sampleName,"CpG",this.minOppStrandCoverage, this.minNextBaseCoverage, this.maxOppStrandAfrac, this.maxNextNonGfrac,chr);
+			PrintWriter outWriter = new PrintWriter(new File(fn));
+			MatUtils.matlabCsv(outWriter, MatUtils.intMatToDouble(cpgCounts));
+			outWriter.close();
+
+			if (this.outputCphs)
+			{
+				fn = String.format("%s%s-%s-minOppCvg%d-minNextCvg%d-maxOppA%.3f-maxNextNonG%.3f-%s.csv", 
+						prefix,sampleName,"CpH",this.minOppStrandCoverage, this.minNextBaseCoverage, this.maxOppStrandAfrac, this.maxNextNonGfrac,chr);
+				outWriter = new PrintWriter(new File(fn));
+				MatUtils.matlabCsv(outWriter, MatUtils.intMatToDouble(cphCounts));
+				outWriter.close();
+			}
+		}
+		double frac = (double)filteredOutCounter/((double)usedCounter+(double)filteredOutCounter);
+		System.err.printf("Lost %f%% due to non-converion filter\n%d CpGs filtered for non-conversion, %d CpGs used (MinConv=%d,UseCpgs=%s)\n",
+				frac*100.0, filteredOutCounter, usedCounter, this.minConv, String.valueOf(this.useCpgsToFilter));
+		System.err.printf("Found %d reads total\n", recCounter);
+		System.err.printf("Cph conversion rate: before filter=%f, after filter=%f\n",
+				100.0 * ((double)numCphConvertedNoFilt/(double)numCphTotalNoFilt),
+				100.0 * ((double)numCphConvertedWithFilt/(double)numCphTotalWithFilt));
+	}
+
+
+	private void incrementCytosineCounters(SortedMap<Integer, Cpg> cytosines,
+			int[][] cpgCounts, int[][] cphCounts) 
+	{
+		incrementCytosineCounter(cytosines, cpgCounts, false);
+		if (this.outputCphs) incrementCytosineCounter(cytosines, cphCounts, true);
+	}
+
+	/**
+	 * @param cytosines
+	 * @param cpgCounts
+	 * @param countCphs If true, we count only CpHs, if false we count only CpGs
+	 */
+	private void incrementCytosineCounter(SortedMap<Integer, Cpg> cytosines,
+			int[][] cpgCounts, boolean countCphs) {
+
+//		System.err.printf("Incrementing counter (cph=%s), total pre = %d\n",countCphs,MatUtils.sumAll(cpgCounts));
+		Iterator<Cpg> cytosineIt = cytosines.values().iterator();
+		int cytosineCount = 0;
+		int goodCount = 0;
+		int cpgCount = 0;
+		int cphCount = 0;
+		CPG: while (cytosineIt.hasNext())
+		{
+			Cpg cytosine = cytosineIt.next();
+			cytosineCount++;
+
+			//System.err.printf("\tCytosine %s\n",cytosine.toStringExpanded());
+			
+			
+			// Only count it if it looks like a true cytosine.  Use a minimum of 5 reads for both cytosine
+			// and CpG determination
+			if ((cytosine.totalReadsOpposite>=this.minOppStrandCoverage) && (cytosine.fracOppositeA() <= this.maxOppStrandAfrac))
+			{
+				//System.err.printf("\t\tCytosine opp cvg>=5 %s\n",cytosine.toStringExpanded());
+
+				if (cytosine.nextBaseTotalReads >= this.minNextBaseCoverage)
+				{
+					//System.err.printf("\t\tCytosine next base cvg>=5 %s\n",cytosine.toStringExpanded());
+
+					boolean trueCpg = (cytosine.fracNextBaseG() >= (1.0-this.maxNextNonGfrac));
+					boolean trueCph = (cytosine.fracNextBaseG() < this.maxNextNonGfrac);
+					
+					if (trueCpg) cpgCount++;
+					if (trueCph) cphCount++;
+
+					// If it's the right kind, count it
+					if ((trueCpg && !countCphs) || (trueCph && countCphs))
+					{
+						if (cytosine.totalReadsCorT(true) < this.maxCoverageOutput)
+						{
+							cpgCounts[cytosine.totalReadsCorT(true)][cytosine.totalReadsC(true)]++;
+							goodCount++;
+						}
+					}
+				}
+			}
+		}
+//		System.err.printf("\tIncremented counter (cph=%s), good cytosines=%d/%d, cpg=%d, cph=%d, total post = %d\n\n",
+//				countCphs,goodCount,cytosineCount,cpgCount, cphCount, MatUtils.sumAll(cpgCounts));
+
+		
+	}
+
+	protected static Cpg findOrCreateCpg(Map<Integer,Cpg> cpgs, int onRefCoord, boolean negStrand, char nextBaseRef)
+	{
+		Cpg cpg = cpgs.get(new Integer(onRefCoord));
+		if (cpg == null)
+		{
+			cpg = new Cpg(onRefCoord,negStrand);
+			cpg.setNextBaseRef(nextBaseRef);
+			cpgs.put(new Integer(onRefCoord), cpg);
+		}
+		return cpg;
+	}
+	
+	
+	protected void incrementCpg(Cpg cpg, char seqChar, boolean nonconvFilter, char nextBaseSeq) 
+	throws Exception
+	{
+		int totalReads = 0, cReads = 0, tReads = 0, cReadsNonconvFilt = 0, agReads = 0, nextBaseGreads = 0, nextBaseTotalReads = 0;
+		
+		switch (seqChar)
+		{
+		case 'N':
+		case '0':
+			break;
+		case 'A':
+		case 'G':
+			agReads = 1;
+			totalReads = 1;
+			break;
+		case 'T':
+			tReads = 1;
+			totalReads = 1;
+			break;
+		case 'C':
+			if (nonconvFilter) cReadsNonconvFilt = 1; else cReads = 1;
+			totalReads = 1;
+			break;
+		default:
+			throw new Exception("Can't recognize seq char: " + seqChar);
+		}
+	
+		// And the next base
+		switch (nextBaseSeq)
+		{
+		case 'N':
+		case '0':
+			break;
+		case 'G':
+			nextBaseTotalReads = 1;
+			nextBaseGreads = 1;
+			break;
+		case 'T':
+		case 'A':
+		case 'C':
+			nextBaseTotalReads = 1;
+			break;
+		default:
+			throw new Exception("Can't recognize seq char: " + seqChar);
+		}
+	
+		cpg.totalReads += totalReads;
+		cpg.cReads += cReads;
+		cpg.tReads += tReads;
+		cpg.cReadsNonconversionFilt += cReadsNonconvFilt;
+		cpg.agReads += agReads;
+		
+		cpg.nextBaseGreads += nextBaseGreads;
+		cpg.nextBaseTotalReads += nextBaseTotalReads;
+	}
+	
+	protected void incrementOppositeCpg(Cpg cpg, char seqChar) 
+	throws Exception
+	{
+		int aReadsOpposite = 0, totalReadsOpposite = 0;
+		
+		
+		switch (seqChar)
+		{
+		case 'N':
+		case '0':
+			break;
+		case 'A':
+			aReadsOpposite = 1;
+			totalReadsOpposite = 1;
+			break;
+		case 'G':
+		case 'T':
+		case 'C':
+			totalReadsOpposite = 1;
+			break;
+		default:
+			throw new Exception("Can't recognize seq char: " + seqChar);
+		}
+
+		cpg.aReadsOpposite += aReadsOpposite;
+		cpg.totalReadsOpposite += totalReadsOpposite;
+	}
+	
+
+	
+}
