ceef87746ef652daa7c959f0cc0a3884f65fccd2
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2011-03-21 20:30:40 +0000

    added capture of ins and del beds
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@625 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/wrap_tophat.pl
+++ perl_utils_usc/wrap_tophat.pl
@@ -10,3 +10,5 @@ $file = scalar(@ARGV) > 3 ? $ARGV[$#ARGV - 1] : $ARGV[$#ARGV];
 $file = basename($file);
 system("mv tophat_out/accepted_hits.bam " . $file. ".tophat_hits.bam");
 system("mv tophat_out/junctions.bed " . $file. ".tophat_junctions.bed");
+system("mv tophat_out/insertions.bed " . $file. ".tophat_insertions.bed");
+system("mv tophat_out/deletions.bed " . $file. ".tophat_deletions.bed");
