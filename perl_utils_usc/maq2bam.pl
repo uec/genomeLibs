87e93b615b7c210abec7c6e3b8558f1bba05bdf2
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2011-03-29 01:21:16 +0000

    sort, mark (not remove) dups in the original src bam
    we still create a seperate nodups file for backward compat.
    
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@641 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/maq2bam.pl
+++ perl_utils_usc/maq2bam.pl
@@ -5,6 +5,8 @@ use strict;
 
 
 my $SAMDIR = "/home/uec-00/shared/production/software/samtools";
+my $PICARDDIR = "/home/uec-00/shared/production/software/picard/default/";
+my $JAVA = "/home/uec-00/shared/production/software/java/default/bin/java";
 
 my $mapFn = $ARGV[0] || die "no input map";
 my $refFa = $ARGV[1] || die "no input genome";
@@ -41,7 +43,8 @@ $curIn = $curOut;
 $curOut = "${mapFnBase}.bam";
 system "${SAMDIR}/samtools view -bt $refFai -o ${curOut} ${curIn}";
 
-# remove dups
+
+# remove dups (legacy mode for bw compat)
 $curIn = $curOut;
 $curOut = "${mapFnBase}.NODUPS.bam";
 system "${SAMDIR}/samtools rmdup -s ${curIn} ${curOut}";
@@ -55,7 +58,7 @@ system "${SAMDIR}/samtools sort ${curIn} ${curOutPrefix}";
 # calmd
 $curIn = $curOut;
 $curOut = "${mapFnBase}.NODUPS.sorted.calmd.bam";
-system "${SAMDIR}/samtools calmd -b ${curIn} ${refFa} > ${curOut}";
+system "${SAMDIR}/samtools calmd -b ${curIn} ${refFa} > ${curOut} 2> /dev/null";
 
 # Set final BAM name
 $finalBamFn = $curOut;
@@ -64,3 +67,18 @@ $finalBamFn = $curOut;
 $curIn = $curOut;
 $curOut = "${mapFnBase}.NODUPS.sorted.calmd.bam.bai";
 system "${SAMDIR}/samtools index ${curIn} ${curOut}";
+
+# sort with picard (no point on making a nodups file seperately, but do it for now)
+$curIn = "${mapFnBase}.bam";
+$curOut = "${mapFnBase}.picard.sorted.bam";
+system "$JAVA -Xmx14g -jar $PICARDDIR/SortSam.jar INPUT=$curIn OUTPUT=$curOut SORT_ORDER=coordinate MAX_RECORDS_IN_RAM=3000000";
+
+# mark dups with picard (no point on making a nodups file seperately, but do it for now)
+$curIn = $curOut;
+$curOut = "${mapFnBase}.picard.nodups.bam";
+system "$JAVA -Xmx14g -jar $PICARDDIR/MarkDuplicates.jar INPUT=$curIn OUTPUT=$curOut METRICS_FILE=met.txt MAX_RECORDS_IN_RAM=3000000";
+
+#replace original bam with dup-marked, sorted, bam
+$curIn = $curOut;
+$curOut = "${mapFnBase}.bam";
+system "mv $curIn $curOut";
