57a449f281770a48e69abbdbc216567aba08c81f
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2011-05-04 19:26:25 +0000

    add bai gen to tophat wrapper
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@670 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/wrap_tophat.pl
+++ perl_utils_usc/wrap_tophat.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 use File::Basename;
-
+my $SAMTOOLS = "/home/uec-00/shared/production/software/samtools/samtools";
 $ENV{'BOWTIE_INDEXES'} = "/home/uec-00/shared/production/genomes/bowtie/";
 $ENV{'PATH'} .= ":/home/uec-00/shared/production/software/tophat/default:/home/uec-00/shared/production/software/bowtie/default";
 $execmd = "tophat " . join(" ", @ARGV);
@@ -12,3 +12,4 @@ system("mv tophat_out/accepted_hits.bam " . $file. ".tophat_hits.bam");
 system("mv tophat_out/junctions.bed " . $file. ".tophat_junctions.bed");
 system("mv tophat_out/insertions.bed " . $file. ".tophat_insertions.bed");
 system("mv tophat_out/deletions.bed " . $file. ".tophat_deletions.bed");
+system("$SAMTOOLS index " . $file. ".tophat_hits.bam");
