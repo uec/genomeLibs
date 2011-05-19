013961f2f9302380f05a4d9410c10ceabf269458
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2011-05-19 20:04:45 +0000

    tidying up
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@682 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/merge_bams.pl
+++ perl_utils_usc/merge_bams.pl
@@ -9,10 +9,16 @@ my $output = shift @ARGV;
 my $cmd = "VALIDATION_STRINGENCY=SILENT MERGE_SEQUENCE_DICTIONARIES=true CREATE_INDEX=true USE_THREADING=true MAX_RECORDS_IN_RAM=3000000 OUTPUT='$output' ";
 for my $file (@ARGV)
 {
-	$cmd .= "INPUT='$file' ";
+        $cmd .= "INPUT='$file' ";
 }
-
 runcmd("$JAVA -Xmx14g -jar $PICARD/MergeSamFiles.jar $cmd");
+
+
+$outputdups = $output;
+$outputdups =~ s/bam$/mdups\.bam/;
+runcmd("$JAVA -Xms14g -Xmx14g -jar $PICARD/MarkDuplicates.jar VALIDATION_STRINGENCY=SILENT METRICS_FILE=dupmets.txt READ_NAME_REGEX=null INPUT=$output OUTPUT=$outputdups");
+
+
 sub runcmd
 {
         my $cmd = shift @_;
