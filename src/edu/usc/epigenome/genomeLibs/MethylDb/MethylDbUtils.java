b89618bb05fb0367c040ce523d177ae9c6fc75f7
benberm <benberm@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2011-06-23 01:00:18 +0000

    Added autosomes
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@694 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- src/edu/usc/epigenome/genomeLibs/MethylDb/MethylDbUtils.java
+++ src/edu/usc/epigenome/genomeLibs/MethylDb/MethylDbUtils.java
@@ -32,6 +32,13 @@ public class MethylDbUtils {
         		, "chrX"
         		);
 	
+	public static final List<String> AUTOSOMES =
+		Arrays.asList(
+				"chr1","chr2","chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", 
+        		"chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", 
+        		"chr19", "chr20", "chr21", "chr22"
+        		);
+	
 	public static final List<String> CHROMS_MINUS_TWELVE =
 		Arrays.asList(
 //				"chrY", "chrM",
