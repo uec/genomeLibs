6b7f1488af29cc825805ba1c6b5c26479a654779
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2011-10-05 19:24:39 +0000

    bwa based contam checked for pipeline
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@747 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

---
+++ perl_utils_usc/orgcontamcheck.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl 
+use File::Basename;
+# Mon Jul 12 14:03:06 PDT 2010
+
+
+my $bwa = "/home/uec-00/shared/production/software/perl_utils_usc/wrap_bwa.pl";
+my $SAMTOOLS = "/home/uec-00/shared/production/software/samtools/samtools";
+my $PICARD = "/home/uec-00/shared/production/software/picard/default";
+my $JAVA = "/home/uec-00/shared/production/software/java/default/bin/java";
+
+my $input = shift @ARGV;
+my $sampleSize = shift @ARGV;
+$lines = $sampleSize * 4;
+
+my $inputBase = basename($input);
+
+runcmd("head -n $lines $input > $inputBase\.sampled.txt");
+
+for my $genome ( @ARGV )
+{
+	my $genomeBase = basename($genome);
+	
+	runcmd("$bwa $genome $inputBase\.sampled.txt $inputBase\.$genomeBase\.bam");
+	runcmd("$SAMTOOLS flagstat  $inputBase\.$genomeBase\.bam > ContamCheck\.$genomeBase\.$sampleSize\.$inputBase\.flagstat.txt");
+}
+
+
+
+sub runcmd
+{
+        my $cmd = shift @_;
+        print STDERR "$cmd\n";
+        system($cmd);
+}
