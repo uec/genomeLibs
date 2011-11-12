2ce519afc9feb4ec2f07019a7d1be2b23d27b28e
benberm <benberm@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2011-11-12 00:35:45 +0000

    Added percentageof human genome.
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@763 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/gtfLengths.pl
+++ perl_utils_usc/gtfLengths.pl
@@ -7,8 +7,15 @@ foreach my $fn (@ARGV)
     my $resa = `wc -l $fn`;
     chomp $resa;
     $resa--;
+
+    # Feat lengths
     my $res = `java GtfFeatLengths $fn 2> /dev/null | tail -1`;
     chomp $res;
     $res =~ s/Total:\s+//g;
-    print sprintf("%s:\t%10d\t%6d\n", $fn, $res, $resa);
+
+    # Percentage of human genome
+    my $perc = 100* ($res / 2.65E9);
+
+
+    print sprintf("%s:\t%10d\t%7.4f%%\t%6d\n", $fn, $res, $perc, $resa);
 }
