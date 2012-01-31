31d146b868909b5fec9534d8bb5bbb2e457fcdcc
dnaase <dnaase@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2012-01-31 05:36:21 +0000

    NDRdetectWalker-0.2, detect NDR by wilcoxon rank sum test
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@797 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- gatkUSCEC/gatkUSCEC/org/broadinstitute/sting/gatk/uscec/YapingWalker/NDRargumentCollection.java
+++ gatkUSCEC/gatkUSCEC/org/broadinstitute/sting/gatk/uscec/YapingWalker/NDRargumentCollection.java
@@ -11,13 +11,22 @@ public class NDRargumentCollection extends BisulfiteArgumentCollection {
 	}
 	
 	@Argument(fullName = "nucleosome_position_window", shortName = "npw", doc = "define the basic nucleosome depletion window size(bp)", required = false)
-    public int nucPosWindow = 150;
+    public int nucPosWindow = 147;
+	
+	@Argument(fullName = "nucleosome_linker_window", shortName = "nlw", doc = "define the basic nucleosome linker window size(bp)", required = false)
+    public int nucLinkerWindow = 40;
 	
 	@Argument(fullName = "minimum_number_gch_in_window_has_methy_value", shortName = "mgn", doc = "minimum number of gch in window has methy value", required = false)
-    public int minGchNum = 5;
+    public int minGchNum = 3;
 	
 	@Argument(fullName = "minimum_CT_depth_for_gch_in_window", shortName = "mcd", doc = "minimum CT reads depth for GCH inside window", required = false)
     public int minCTDepth = 3;
+	
+	@Argument(fullName = "minimum_number_gch_in_linker_window_has_methy_value", shortName = "mgnlw", doc = "minimum number of gch in linker window has methy value", required = false)
+    public int minGchNumLinkerWindow = 2;
+	
+	@Argument(fullName = "minimum_CT_depth_for_gch_in_linker_window", shortName = "mcdlw", doc = "minimum CT reads depth for GCH insidelinker  window", required = false)
+    public int minCTDepthLinkerWindow = 3;
 	
 	@Argument(fullName = "wig_output", shortName = "wo", doc = "wig File to which variants should be written", required = true)
     public String wigFile = null;
@@ -28,19 +37,26 @@ public class NDRargumentCollection extends BisulfiteArgumentCollection {
 	@Argument(fullName = "minimum_gch_methy_diff_for_ndr", shortName = "ndrDiffThreshold", doc = "minimum GCH methylation value differences with adjacent window to be identified as NDR region", required = false)
     public double ndrDiffThreshold = 0.4;
 	
-	@Argument(fullName = "enable_ks_test_for_ndr", shortName = "ksTest", doc = "enable KS test rather than hard threshold to detect NDR region", required = false)
-    public boolean ksTest = false;
+	@Argument(fullName = "enable_stat_test_for_ndr", shortName = "statTest", doc = "enable KS test rather than hard threshold to detect NDR region", required = false)
+    public boolean statTest = false;
+	
+	@Argument(fullName = "sig_threshold_for_test", shortName = "sigValue", doc = "significance threshold to detect NDR region", required = false)
+    public double sigValue = 0.01;
 	
 	
 	public NDRargumentCollection clone() {
 		NDRargumentCollection nac = new NDRargumentCollection();
 		nac.nucPosWindow = nucPosWindow;
+		nac.nucLinkerWindow = nucLinkerWindow;
 		nac.minGchNum = minGchNum;
 		nac.minCTDepth = minCTDepth;
+		nac.minGchNumLinkerWindow = minGchNumLinkerWindow;
+		nac.minCTDepthLinkerWindow = minCTDepthLinkerWindow;
 		nac.wigFile = wigFile;
 		nac.ndrThreshold = ndrThreshold;
 		nac.ndrDiffThreshold = ndrDiffThreshold;
-		nac.ksTest = ksTest;
+		nac.statTest = statTest;
+		nac.sigValue = sigValue;
 		
 		return nac;
 	}
--- gatkUSCEC/gatkUSCEC/org/broadinstitute/sting/gatk/uscec/YapingWalker/NDRdetectWalker.java
+++ gatkUSCEC/gatkUSCEC/org/broadinstitute/sting/gatk/uscec/YapingWalker/NDRdetectWalker.java
@@ -1,8 +1,13 @@
 package org.broadinstitute.sting.gatk.uscec.YapingWalker;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
+
+
+import jsc.independentsamples.MannWhitneyTest;
+import jsc.tests.H1;
 
 import org.apache.log4j.Logger;
 import org.broadinstitute.sting.commandline.ArgumentCollection;
@@ -34,13 +39,14 @@ import org.broadinstitute.sting.utils.pileup.PileupElement;
 import org.broadinstitute.sting.utils.pileup.ReadBackedPileup;
 import org.broadinstitute.sting.utils.pileup.ReadBackedPileupImpl;
 import org.broadinstitute.sting.utils.sam.GATKSAMRecord;
+import org.broadinstitute.sting.gatk.uscec.YapingWalker.NDRdetectWalker.windowsObject;
 
 @BAQMode(QualityMode = BAQ.QualityMode.OVERWRITE_QUALS, ApplicationTime = BAQ.ApplicationTime.ON_INPUT)
 @Reference(window=@Window(start=-200,stop=200))
 @By(DataSource.REFERENCE)
 @Downsample(by=DownsampleType.NONE)
-public class NDRdetectWalker extends LocusWalker<NDRCallContext,LinkedList<NDRCallContext>> implements
-		TreeReducible<LinkedList<NDRCallContext>> {
+public class NDRdetectWalker extends LocusWalker<NDRCallContext,windowsObject> implements
+		TreeReducible<windowsObject> {
 
 	@ArgumentCollection private static NDRargumentCollection NAC = new NDRargumentCollection();
 	
@@ -49,7 +55,7 @@ public class NDRdetectWalker extends LocusWalker<NDRCallContext,LinkedList<NDRCa
 	
 	//private NDRdetectionEngine NDRD_engine = null;
 	
-	private LinkedList<NDRCallContext> windows = null;
+	private windowsObject windows = null;
 	
 	private GenotypePriors genotypePriors;
 	
@@ -137,7 +143,7 @@ public class NDRdetectWalker extends LocusWalker<NDRCallContext,LinkedList<NDRCa
 		BisSNPUtils it = new BisSNPUtils(NAC);
 		AlignmentContext stratifiedContexts = it.getFilteredAndStratifiedContexts(NAC, ref, context);
 		
-		NDRCallContext ncc = new NDRCallContext(context, ref.getLocus());
+		NDRCallContext ncc = new NDRCallContext(context, ref.getLocus()); // this make some biad for seq poor region's nuc window 
 		
 		if(stratifiedContexts == null){
 			ncc.setCytosinePatternFlag(false);
@@ -160,37 +166,101 @@ public class NDRdetectWalker extends LocusWalker<NDRCallContext,LinkedList<NDRCa
 	}
 
 	@Override
-	public LinkedList<NDRCallContext> reduce(NDRCallContext value, LinkedList<NDRCallContext> windows) {
+	public windowsObject reduce(NDRCallContext value, windowsObject windows) {
 		// TODO Auto-generated method stub
 		if ( value == null )
             return windows;
-		if(!windows.isEmpty()){
-			if(!value.getLoc().onSameContig(windows.getFirst().getLoc())){
-				windows.clear();
-				windows.addLast(value);
-				return windows;
+
+		if(windows.windowsPre.size() <= NAC.nucLinkerWindow - 1){
+			if(windows.windowsPre.peekLast() != null){
+				if(value.getLoc().discontinuousP(windows.windowsPre.peekLast().getLoc())){
+					windows.windowsPre.clear();
+					windows.windowsMid.clear();
+					windows.windowsPost.clear();
+				}
 			}
-			else if(value.getLoc().discontinuousP(windows.getLast().getLoc())){
-				windows.clear();
-				windows.addLast(value);
-				return windows;
-			}
-		}
-		
-		
-		if(windows.size() <= NAC.nucPosWindow - 2){
-			windows.addLast(value);
+			windows.windowsPre.offerLast(value);
 			return windows;
 		}	
-		else if(windows.size() == NAC.nucPosWindow - 1){
-			windows.addLast(value);
+		else if(windows.windowsMid.size() <= NAC.nucPosWindow - 2){
+			if(windows.windowsMid.peekLast() != null){
+				if(value.getLoc().discontinuousP(windows.windowsMid.peekLast().getLoc())){
+					windows.windowsPre.clear();
+					windows.windowsMid.clear();
+					windows.windowsPost.clear();
+					windows.windowsPre.offerLast(value);
+					
+					return windows;
+				}
+			}
+			windows.windowsMid.offerLast(value);
+			return windows;
+		}
+		else if(windows.windowsMid.size() == NAC.nucPosWindow - 1){
+			if(windows.windowsMid.peekLast() != null){
+				if(value.getLoc().discontinuousP(windows.windowsMid.peekLast().getLoc())){ // there is potential bias here for not enough reads in postWindow..
+					windows.windowsPre.clear();
+					windows.windowsMid.clear();
+					windows.windowsPost.clear();
+					windows.windowsPre.offerLast(value);
+					
+					return windows;
+				}
+			}
+			windows.windowsMid.offerLast(value);
+		}
+		else if(windows.windowsPost.size() <= NAC.nucLinkerWindow - 2){
+			if(windows.windowsPost.peekLast() != null){
+				if(value.getLoc().discontinuousP(windows.windowsPost.peekLast().getLoc())){
+					windows.windowsPre.clear();
+					windows.windowsMid.clear();
+					windows.windowsPost.clear();
+					windows.windowsPre.offerLast(value);
+					
+					return windows;
+				}
+			}
+			windows.windowsPost.offerLast(value);
+			return windows;
+		}
+		else if(windows.windowsPost.size() == NAC.nucLinkerWindow - 1){
+			if(windows.windowsPost.peekLast() != null){
+				if(value.getLoc().discontinuousP(windows.windowsPost.peekLast().getLoc())){
+					windows.windowsPre.clear();
+					windows.windowsMid.clear();
+					windows.windowsPost.clear();
+					windows.windowsPre.offerLast(value);
+					
+					return windows;
+				}
+			}
+			windows.windowsPost.offerLast(value);
 		}
 		else{
-			windows.removeFirst();
-			windows.addLast(value);
+			if(windows.windowsPost.peekLast() != null){
+				if(windows.windowsMid.peekLast().getLoc().discontinuousP(windows.windowsPost.peekFirst().getLoc())){
+					windows.windowsPre.clear();
+					windows.windowsMid.clear();
+					windows.windowsPost.clear();
+					windows.windowsPre.offerLast(value);
+					
+					return windows;
+				}
+			}
+			windows.windowsPre.pollFirst();
+			windows.windowsPre.offerLast(windows.windowsMid.pollFirst());
+			windows.windowsMid.offerLast(windows.windowsPost.pollFirst());
+			windows.windowsPost.offerLast(value);
+			
 		}
 		summary.nWindowsVisited++;
-		getValueFromWindow(windows);
+		if(NAC.statTest){
+			getNDRFromWindowsBySigTest(windows);
+		}
+		else{
+		//	getValueFromWindow(windows);
+		}
+	//	
 		//GenomeLoc locMidInWind = window.get(NAC.nucPosWindow/2).getLocation();
 		
 		
@@ -198,20 +268,20 @@ public class NDRdetectWalker extends LocusWalker<NDRCallContext,LinkedList<NDRCa
 	}
 
 	@Override
-	public LinkedList<NDRCallContext> reduceInit() {
+	public windowsObject reduceInit() {
 		// TODO Auto-generated method stub
-		windows = new LinkedList<NDRCallContext>();
+		windows = new windowsObject();
 		return windows;
 	}
 	
 	@Override
-	public LinkedList<NDRCallContext> treeReduce(LinkedList<NDRCallContext> lhWinds,
-			LinkedList<NDRCallContext> rhWinds) {
+	public windowsObject treeReduce(windowsObject lhWinds,
+			windowsObject rhWinds) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	
-	public void onTraversalDone(LinkedList<NDRCallContext> sum) {
+	public void onTraversalDone(windowsObject sum) {
 		logger.info(String.format("Visited windows                                %d", summary.nWindowsVisited));
 		logger.info(String.format("Callable windows                                %d", summary.nWindowsCallable));
 		logger.info(String.format("Confidantly called NDR windows                                %d", summary.nNDRWindowsCalledConfidently));
@@ -224,6 +294,55 @@ public class NDRdetectWalker extends LocusWalker<NDRCallContext,LinkedList<NDRCa
 		logger.info(String.format("Average GCH CT reads depth in NDR windows                                %.2f", summary.percentGchCTDepthOfNDRWindows()));
 		logger.info(String.format("Average GCH number in NDR windows                                %.2f", summary.percentGchNumOfNDRWindows()));
 		writer.close();
+	}
+	// by statistics test
+	public void getNDRFromWindowsBySigTest(windowsObject windows){
+		
+		
+		windowsReturnObject objMid = getGchListFromWindow(windows.windowsMid);	
+		windowsReturnObject objPre = getGchListFromWindow(windows.windowsPre);		
+		windowsReturnObject objPost = getGchListFromWindow(windows.windowsPost);
+		
+		double averageGchMethy = Double.NaN;
+		
+		
+		if(objMid.numValidGch >= NAC.minGchNum){
+			summary.sumGchNumInWindCalledConfidently += objMid.numValidGch;
+			summary.sumGchCTDepthWind += objMid.sumGchCTDepth/(double)objMid.numValidGch;
+			averageGchMethy = objMid.sumGchMethy/(double)objMid.numValidGch;
+		}
+		
+		if(!Double.isNaN(averageGchMethy)){
+			summary.nWindowsCallable++;
+			summary.sumGchMethyWindowsCalledConfidently += averageGchMethy;
+			//windows.getLast().setGchMethyInWindow(averageGchMethy);
+			if(objPre.numValidGch >= NAC.minGchNumLinkerWindow){
+				double sigValue = getSigTest(objMid.dot, objPre.dot);
+				if(sigValue < NAC.sigValue){
+					summary.nNDRWindowsCalledConfidently++;
+					summary.sumGchMethyNDRWindowsCalledConfidently += averageGchMethy;
+					summary.sumGchNumInNDRWind += objMid.numValidGch;
+					summary.sumGchCTDepthInNDRWind += objMid.sumGchCTDepth/(double)objMid.numValidGch;
+					writer.add(windows.windowsMid.getLast().getLoc(), averageGchMethy);
+					System.out.println("pre: " + windows.windowsMid.getFirst().getLoc().getStart() + "\t" + averageGchMethy + "\t" + sigValue + "\t" + objPre.sumGchMethy/(double)objPre.numValidGch + "\t" + objPre.numValidGch+ "\t" + objPre.sumGchCTDepth/(double)objPre.numValidGch);
+				}
+			}
+			else if(objPost.numValidGch >= NAC.minGchNumLinkerWindow){
+				double sigValue = getSigTest(objMid.dot, objPost.dot);
+				if(sigValue < NAC.sigValue){
+					summary.nNDRWindowsCalledConfidently++;
+					summary.sumGchMethyNDRWindowsCalledConfidently += averageGchMethy;
+					summary.sumGchNumInNDRWind += objMid.numValidGch;
+					summary.sumGchCTDepthInNDRWind += objMid.sumGchCTDepth/(double)objMid.numValidGch;
+					writer.add(windows.windowsMid.getLast().getLoc(), averageGchMethy);
+					System.out.println("post: " + windows.windowsMid.getFirst().getLoc().getStart() + "\t" + averageGchMethy + "\t" + sigValue + "\t" + objPost.sumGchMethy/(double)objPost.numValidGch+ "\t" + objPost.numValidGch+ "\t" + objPost.sumGchCTDepth/(double)objPost.numValidGch);
+				}
+				
+			}
+			
+			
+		}
+		
 	}
 
 	public void getValueFromWindow(LinkedList<NDRCallContext> windows){
@@ -310,13 +429,102 @@ public class NDRdetectWalker extends LocusWalker<NDRCallContext,LinkedList<NDRCa
 		public LinkedList<NDRCallContext> windowsMid = null;
 		public LinkedList<NDRCallContext> windowsPre = null;
 		public LinkedList<NDRCallContext> windowsPost = null;
+		public boolean windowsPreContinuous = true;
+		public boolean windowsPostContinuous = true;
 		public windowsObject(){
 			windowsMid = new LinkedList<NDRCallContext>();
 			windowsPre = new LinkedList<NDRCallContext>();
 			windowsPost = new LinkedList<NDRCallContext>();
 		}
-		
 	
 	}
+	
+	public windowsReturnObject getGchListFromWindow(LinkedList<NDRCallContext> window){
+		double sumGchMethy = 0;
+		int numValidGch = 0;
+		int sumGchCTDepth = 0;
+		ArrayList<Double> dot = new ArrayList<Double>();
+		Iterator<NDRCallContext> itContext = window.iterator();
+		while(itContext.hasNext()){
+			NDRCallContext tmpContext = itContext.next();
+			if(!tmpContext.getCytosinePatternFlag())
+				continue;
+			int numC = 0;
+			int numT = 0;
+			for( PileupElement p : tmpContext.getRealContext().getBasePileup()){
+				boolean negStrand = p.getRead().getReadNegativeStrandFlag();
+				if(((GATKSAMRecord)p.getRead()).isGoodBase(p.getOffset())){
+					if(negStrand){
+						if(p.getBase()==BaseUtils.G){
+							numC++;
+						}
+						else if(p.getBase()==BaseUtils.A){
+							numT++;
+						}
+						else{
+							
+						}
+						
+					}
+					else{
+						if(p.getBase()==BaseUtils.C){
+							numC++;
+						}
+						else if(p.getBase()==BaseUtils.T){
+							numT++;
+						}
+						else{
+							
+						}
+					}
+				}
+				//if ( BisSNPUtils.usableBase(p, true) ){
+					//sumGchSeqDepth++;
+				//	if(p.getBase() == BaseUtils.C)
+				//		numC++;
+				//	if(p.getBase() == BaseUtils.T)
+				//		numT++;
+		        //}
+			}
+			if((numC + numT) >= NAC.minCTDepth){
+				numValidGch++;
+				sumGchCTDepth += (numC + numT);
+				sumGchMethy += (double)numC/(double)(numC + numT);
+				dot.add((double)numC/(double)(numC + numT));
+			//	System.out.println("loc: " + tmpContext.getLoc().getStart() + "\tGchMethy: " + (double)numC/(double)(numC + numT) + "\tnumC: " + numC + "\tnumT: " + numT);
+			}
+		}
+		windowsReturnObject obj = new windowsReturnObject(numValidGch, sumGchCTDepth, sumGchMethy, dot);
+		return obj;
+	}
 
+	public class windowsReturnObject {
+		public int numValidGch;
+		public double sumGchCTDepth;
+		public double sumGchMethy;
+		public ArrayList<Double> dot;
+		public windowsReturnObject(int numValidGch,int sumGchCTDepth,double sumGchMethy, ArrayList<Double> dot){
+			this.numValidGch = numValidGch;
+			this.sumGchCTDepth = sumGchCTDepth;
+			this.sumGchMethy = sumGchMethy;
+			this.dot = dot;
+		}
+	}
+	
+	
+	//using MannWhitneyTest/Wilcox rank sum test
+	public double getSigTest(ArrayList<Double> dotMid, ArrayList<Double> dotAdj){
+		
+		double[] mid = new double[dotMid.size()];
+		double[] adj = new double[dotAdj.size()];
+		for(int i = 0; i < dotMid.size();i++){
+			mid[i] = dotMid.get(i);
+		}
+		for(int i = 0; i < dotAdj.size();i++){
+			adj[i] = dotAdj.get(i);
+		}
+		MannWhitneyTest test = new MannWhitneyTest(mid, adj, H1.GREATER_THAN);
+		
+		return test.getSP();
+	}
 }
