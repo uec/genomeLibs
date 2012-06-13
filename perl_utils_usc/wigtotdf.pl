853ee12a3a0e68abbcb85925eee8d8b3fe9ddc92
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2012-06-13 23:07:54 +0000

    added check for IGV genomes in wigtoTDF
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@826 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/wigtotdf.pl
+++ perl_utils_usc/wigtotdf.pl
@@ -1,13 +1,18 @@
 #!/usr/bin/perl
 #CONSTANTS
-$igvtools = "/home/uec-00/shared/production/software/igvtools/igvtools";
+$igvtools = "/home/uec-00/shared/production/software/igvtools/default/igvtools";
 
 
 $input = $ARGV[0] || die "specify input file";
 $output = $ARGV[1] || die "specify input file";
-$genome = $ARGV[2] || die "specify input file";
+$ref = $ARGV[2] || die "specify input file";
 
 
-$cmd = "$igvtools tile -c $input $output $genome";
+$genome = "hg19";
+$genome = "mm9" if $ref =~ /mm9/;
+$genome = "hg18" if $ref =~ /hg18/;
+$genome = "rn4" if $ref =~ /rn4/;
+
+$cmd = "$igvtools toTDF  $input $output $genome";
 print "$cmd\n";
 system($cmd);
