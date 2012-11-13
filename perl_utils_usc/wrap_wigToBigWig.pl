fdc2640fbfdf00c42c90af0429286554dc832a78
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2012-11-13 20:25:56 +0000

    added wig to bigwig wrapper
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@830 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

---
+++ perl_utils_usc/wrap_wigToBigWig.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+$ucsc = "/auto/uec-00/shared/production/software/UCSC_Browser_Tools/default";
+
+$input = $ARGV[0] || die "specifiy wig file";
+$input =~ /wig/i || die "specifiy wig file";
+$output = $ARGV[1] || die "specifiy output file";
+
+$genome = $ARGV[2] || "hg19";
+$genome = "mm9" if $input =~ /mm9/;
+$genome = "hg18" if $input =~ /hg18/;
+
+
+system("$ucsc/wigToBigWig -clip $input $ucsc/$genome\.chrom.sizes $output");
