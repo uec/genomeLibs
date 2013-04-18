d9fb73d1880fc315d0b4e3e4a3c6c7919587b948
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2013-04-18 03:03:52 +0000

    fixed tophat2 wrapper to produce picard compatible bams
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@846 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/wrap_tophat2.pl
+++ perl_utils_usc/wrap_tophat2.pl
@@ -7,10 +7,8 @@ $ENV{'PATH'} .= ":/home/uec-00/shared/production/software/tophat2/default:/home/
 $file = scalar(@ARGV) > 4 ? $ARGV[$#ARGV - 1] : $ARGV[$#ARGV];
 $file = basename($file);
 
-
 my $phred = `/home/uec-00/shared/production/software/perl_utils_usc/testFastqQualityScale.pl $file`;
 $phred = $phred =~ /64/ ? "--solexa1.3-quals" : "";
-
 
 $execmd = "tophat2 $phred --no-coverage-search " . join(" ", @ARGV);
 print "$execmd\n";
@@ -20,6 +18,9 @@ system("mv tophat_out/unmapped.bam " . $file. ".unmapped.bam");
 system("mv tophat_out/junctions.bed " . $file. ".tophat_junctions.bed");
 system("mv tophat_out/insertions.bed " . $file. ".tophat_insertions.bed");
 system("mv tophat_out/deletions.bed " . $file. ".tophat_deletions.bed");
-system("$SAMTOOLS merge tmp.bam " . $file. ".tophat_hits.bam " . $file. ".unmapped.bam");
-system("mv tmp.bam " . $file. ".tophat_hits.bam");
+system("$SAMTOOLS merge tmp1.bam " . $file. ".tophat_hits.bam " . $file. ".unmapped.bam");
+system("$SAMTOOLS sort -n tmp1.bam tmp2");
+system("$SAMTOOLS fixmate tmp2.bam tmp3.bam");
+system("$SAMTOOLS sort tmp3.bam tmp4");
+system("mv tmp4.bam " . $file. ".tophat_hits.bam");
 system("$SAMTOOLS index " . $file. ".tophat_hits.bam");
