fedb9f5b4e8a1a5cc725e4b0cf5a579eb0ee6a5f
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2013-05-01 21:03:21 +0000

    fixed version tracking
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@851 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/trackversions.pl
+++ perl_utils_usc/trackversions.pl
@@ -7,10 +7,13 @@ my %progs;
 $progs{SAMTOOLS} = "/home/uec-00/shared/production/software/samtools/samtools";
 $progs{PICARD} = "/home/uec-00/shared/production/software/picard/default/";
 $progs{JAVA} = "/home/uec-00/shared/production/software/java/default/bin/java";
-$progs{BWA} = "/home/uec-00/shared/production/software/bwa/default/";
+$progs{BWA} = "/home/uec-00/shared/production/software/bwa/latest/";
 $progs{TOPHAT} = "/home/uec-00/shared/production/software/tophat/default/";
+$progs{TOPHAT2} = "/home/uec-00/shared/production/software/tophat2/default/";
 $progs{BOWTIE} = "/home/uec-00/shared/production/software/bowtie/default/";
+$progs{BOWTIE2} = "/home/uec-00/shared/production/software/bowtie2/default/";
 $progs{CUFFLINKS} = "/home/uec-00/shared/production/software/cufflinks/default/";
+$progs{CUFFLINKS} = "/home/uec-00/shared/production/software/cufflinks2/default/";
 $progs{UEC_GATK} = "/home/uec-00/shared/production/software/uecgatk/default/";
 $progs{BSMAP} = "/home/uec-00/shared/production/software/bsmap/default/";
 
@@ -19,9 +22,9 @@ open(OUT, ">$output");
 
 for $app (keys %progs)
 {
-        print OUT "$app=";
-        print OUT abs_path($progs{$app});
-        print OUT "\n";
+	print OUT "$app=";
+	print OUT abs_path($progs{$app});
+	print OUT "\n";
 }
 
 close OUT;
