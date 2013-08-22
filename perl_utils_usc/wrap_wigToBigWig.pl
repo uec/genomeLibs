d34ad35cc097bf9e73721984232815ff42fb53a6
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2013-08-22 18:41:13 +0000

    added mm10
    had to created coressponding mm10.chrom.sizes in ucsc dir
    
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@858 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/wrap_wigToBigWig.pl
+++ perl_utils_usc/wrap_wigToBigWig.pl
@@ -8,6 +8,7 @@ $output = $ARGV[1] || die "specifiy output file";
 
 $genome = $ARGV[2] || "hg19";
 $genome = "mm9" if $input =~ /mm9/;
+$genome = "mm10" if $input =~ /mm10/;
 $genome = "hg18" if $input =~ /hg18/;
 
 system("$ucsc/wigToBigWig -clip $input $ucsc/$genome\.chrom.sizes $output");
