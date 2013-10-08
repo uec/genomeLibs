af91a2e5f86c5015ff6e8440737a96c8b8251c6d
zack_hr <zack_hr@2b8d95ea-eb58-0410-8a55-29fbd2c437c3>
2013-10-08 19:38:05 +0000

    added genomes to wig2bigwig
    git-svn-id: https://svn.code.sf.net/p/ngsgenomelibs/code/trunk@859 2b8d95ea-eb58-0410-8a55-29fbd2c437c3

--- perl_utils_usc/wrap_wigToBigWig.pl
+++ perl_utils_usc/wrap_wigToBigWig.pl
@@ -10,5 +10,7 @@ $genome = $ARGV[2] || "hg19";
 $genome = "mm9" if $input =~ /mm9/;
 $genome = "mm10" if $input =~ /mm10/;
 $genome = "hg18" if $input =~ /hg18/;
+$genome = "rn5" if $input =~ /rn5/;
+$genome = "danRer7" if $input =~ /zv9/i;
 
 system("$ucsc/wigToBigWig -clip $input $ucsc/$genome\.chrom.sizes $output");
